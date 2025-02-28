eac36e40633547c56c0be9b5a2ce233d5d5ffacf
GoJiMin <gojimin3095@gmail.com>
2025-03-01 03:29:35 +0900

    [chore] AWS 프리티어 환경에서 Nginx + Blue-Green 배포 방식을 통한 무중단 배포 구현
    - AWS 프리티어 환경에서 pm2 클러스터 모드가 불가능한 문제를 해결하기 위해, Nginx와 Blue-Green 배포 방식을 활용해 무중단 배포 구현.
    - 3000/3001 포트를 스위칭해 서버를 실행시키며, 심볼릭 링크를 활용해 배포에 실패해도 기존 프로세스를 유지하도록 구현.
    - 서버 실행 후 Health Check을 통해 HTTP 200 응답이 확인되지 않으면 배포를 즉시 중단하도록 처리.
    - Health Check 성공 후 Nginx의 프록시 포트를 변경 후 기존 프로세스 종료.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,34 +1,83 @@
 #!/bin/bash
+source ~/.bashrc
 
-PROJECT_ROOT="/home/ubuntu/deploy"
-PROJECT_NAME='Modu-Review-Client'
-NVM_DIR="/home/ubuntu/.nvm"
+DEPLOY_PATH="/home/ubuntu/deploy"
 
-current_time=$(date "+%y-%m-%d-%H-%M-%S")
-DeployLogFile="/home/ubuntu/log/DeployLog_$current_time.log"
-
-cd $PROJECT_ROOT
-
-echo "Loading NVM environment : $current_time" >> $DeployLogFile
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-echo "" && echo "" >> $DeployLogFile 2>&1
+PROJECT_ROOT="/home/ubuntu/releases"
+CURRENT_TIME=$(date "+%y-%m-%d-%H-%M-%S")
+RELEASE_PATH="$PROJECT_ROOT/$CURRENT_TIME"
 
 PM2_PATH="/home/ubuntu/.nvm/versions/node/v22.14.0/bin/pm2"
 
-echo "Modu-Review-Client Deploy : $current_time" >> $DeployLogFile
+SYMLINK_PATH="/home/ubuntu/application"
+PROJECT_NAME='Modu-Review-Client'
 
-echo "Install Dependencies : $current_time" >> $DeployLogFile
-pnpm install >> $DeployLogFile 2>&1
-echo "" && echo "" >> $DeployLogFile 2>&1
+LOG_FILE="/home/ubuntu/log/DeployLog_$CURRENT_TIME.log"
 
-echo "Shut down an existing server : $current_time" >> $DeployLogFile
-$PM2_PATH delete $PROJECT_NAME >> $DeployLogFile 2>&1
+NVM_DIR="/home/ubuntu/.nvm"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-echo "Run the server : $current_time" >> $DeployLogFile
-$PM2_PATH start "pnpm start" --name $PROJECT_NAME
-echo "" && echo "" >> $DeployLogFile 2>&1
+echo "현재 실행 중인 포트를 확인합니다. : $CURRENT_TIME" >> $LOG_FILE
+CURRENT_PORT=$(sed -n "s/^set \$service_url http:\/\/$SERVER_IP:\([0-9]*\);/\1/p" /etc/nginx/conf.d/service-url.inc)
+echo "" && echo "" >> $LOG_FILE 2>&1
 
-$PM2_PATH ps && $PM2_PATH ps >> $DeployLogFile 2>&1
+if [ $CURRENT_PORT -eq 3000 ]; then
+    NEW_PORT=3001
+    OLD_NAME="next-blue"
+    NEW_NAME="next-green"
+else
+    NEW_PORT=3000
+    OLD_NAME="next-green"
+    NEW_NAME="next-blue"
+fi
 
-echo "Successful deployment and server execution: $current_time" >> $DeployLogFile 2>&1
+echo "모두의 리뷰 새로운 버전을 $NEW_PORT 포트에서 실행합니다. : $CURRENT_TIME" >> $LOG_FILE
+echo "" && echo "" >> $LOG_FILE 2>&1
+
+mv $DEPLOY_PATH $RELEASE_PATH
+mkdir $DEPLOY_PATH
+ln -sfn $RELEASE_PATH $SYMLINK_PATH
+cd $SYMLINK_PATH
+
+echo "필요한 의존성을 설치합니다. : $CURRENT_TIME" >> $LOG_FILE
+pnpm install >> $LOG_FILE 2>&1
+echo "" && echo "" >> $LOG_FILE 2>&1
+
+echo "서버를 실행합니다. : $CURRENT_TIME" >> $LOG_FILE
+$PM2_PATH start "node_modules/next/dist/bin/next" --name $NEW_NAME -- --port $NEW_PORT >> $LOG_FILE
+echo "" && echo "" >> $LOG_FILE 2>&1
+
+echo "Health Check를 수행합니다. : $CURRENT_TIME" >> $LOG_FILE
+HEALTH_CHECK_SUCCESS=false
+
+for i in {1..20}; do
+    echo "Health Check 시도 $i / 20" >> $LOG_FILE
+    status_code=$(curl -s -o /dev/null -w "%{http_code}" http://$SERVER_IP:$NEW_PORT)
+    echo "Health Check HTTP 상태 코드: $status_code" >> $LOG_FILE
+    
+    if [ $status_code -eq 200 ]; then
+        echo "새로운 버전이 정상적으로 작동 중입니다. : $CURRENT_TIME" >> $LOG_FILE
+        HEALTH_CHECK_SUCCESS=true
+        break
+    else
+        echo "Health Check에 실패해 5초 후 재시도합니다. : $CURRENT_TIME" >> $LOG_FILE
+        sleep 5
+    fi
+done
+
+if [ $HEALTH_CHECK_SUCCESS = false ]; then
+    echo "Health Check에 모두 실패해 배포를 중단합니다. : $CURRENT_TIME" >> $LOG_FILE
+    $PM2_PATH delete $NEW_NAME >> $LOG_FILE 2>&1
+    exit 1
+fi
+
+echo "" && echo "" >> $LOG_FILE 2>&1
+
+echo "Nginx 프록시 포트를 $CURRENT_PORT 에서 $NEW_PORT 로 변경합니다 : $CURRENT_TIME" >> $LOG_FILE
+echo "set \$service_url http://$SERVER_IP:${NEW_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+sudo service nginx reload
+echo "" && echo "" >> $LOG_FILE 2>&1
+
+echo "기존 서버를 종료합니다 : $CURRENT_TIME" >> $LOG_FILE
+$PM2_PATH delete $OLD_NAME >> $LOG_FILE 2>&1
