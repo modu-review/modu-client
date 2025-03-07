932752cb786b2fa3d56b9fdba5f24f3fb20bf913
queenJiJi <jiyeon.park@mail.mcgill.ca>
2025-03-07 12:48:29 +0900

    [style] Hero Component 퍼블리싱 - 반응형 구현
    Co-authored-by: GoJiMin <gojimin3095@gmail.com>

--- src/views/main/ui/Hero.tsx
+++ src/views/main/ui/Hero.tsx
@@ -4,16 +4,22 @@ import SearchBar from './SearchBar';
 
 export default function Hero() {
   return (
-    <section>
-      <article>
+    <section className="flex flex-col justify-center items-center mt-12">
+      <article className="text-center font-semibold mb-8 text-[18px] md:text-[26px] lg:text-[32px]">
         <p>내가 경험하지 못한 것을 누군가는 먼저 경험했습니다.</p>
         <p>세상의 모든 후기를</p>
-        <p>바로 여기 {'모두의 후기'} 에서 확인하세요.</p>
+        <p>
+          바로 여기 <span className="text-boldBlue">{"'모두의 : 후기'"}</span>에서 확인하세요.
+        </p>
       </article>
       <Link href="/">
-        <Button className="">{'내 경험 공유하기>'}</Button>
+        <Button className="mb-10 bg-boldBlue rounded-3xl hover:bg-extraboldBlue text-[12px] md:text-[16px] md:p-6 lg:text-[18px]">
+          {'내 경험 공유하기>'}
+        </Button>
       </Link>
-      <SearchBar />
+      <div className="flex justify-center w-full px-4">
+        <SearchBar />
+      </div>
     </section>
   );
 }
--- src/views/main/ui/SearchBar.tsx
+++ src/views/main/ui/SearchBar.tsx
@@ -5,9 +5,9 @@ import {LucideIcon} from '@/shared/ui/icons';
 
 export default function SearchBar() {
   return (
-    <div>
+    <div className="flex items-center w-full max-w-[480px] md:max-w-[550px] lg:max-w-[712px] h-full shadow-md rounded-3xl border px-4">
       <LucideIcon name="Search" size={24} color="#53587E" />
-      <Input type="text" placeholder="후기를 검색하세요" />
+      <Input className="ml-3 py-2 text-base border-none outline-none" type="text" placeholder="후기를 검색하세요" />
     </div>
   );
 }
