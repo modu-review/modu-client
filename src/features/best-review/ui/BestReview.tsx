13cdbe4b658dd8583e8c0ff13f0f26535e91a8ed
queenJiJi <jiyeon.park@mail.mcgill.ca>
2025-03-11 11:17:56 +0900

    [refactor] 불필요한 cardlist 추상화 제거
    Co-authored-by: GoJiMin <gojimin3095@gmail.com>

--- src/features/best-review/ui/BestReview.tsx
+++ src/features/best-review/ui/BestReview.tsx
@@ -1,4 +1,8 @@
-import {CardList, CategoryBar} from '@/shared/ui/components';
+'use client';
+
+import {CategoryBar} from '@/shared/ui/components';
+import BestReviewCard from './BestReviewCard';
+
 const MOCK = [
   {
     board_id: 'abcde1232',
@@ -66,7 +70,13 @@ export default function BestReview() {
   return (
     <section className="bg-boldBlue mt-16 py-24 px-8">
       <CategoryBar />
-      <CardList cards={MOCK} from={'mainpage'} />
+      <ul className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-20 content-center justify-items-center">
+        {MOCK.map(card => (
+          <li key={card.board_id}>
+            <BestReviewCard card={card} />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
--- src/shared/ui/components/index.ts
+++ src/shared/ui/components/index.ts
@@ -1,3 +1,2 @@
 export {default as CategoryBar} from './CategoryBar';
-export {default as CardList} from './CardList';
 export {default as CardFrame} from './CardFrame';
