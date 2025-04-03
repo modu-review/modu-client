47bbaa2092bc33aa193fc623e2f4f596a5d78afa
GoJiMin <gojimin3095@gmail.com>
2025-04-03 18:14:30 +0900

    [feat] 로그인 링크 연결
    - 로컬 테스트를 위해 redirect_uri를 localhost로 연결.

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,16 +1,19 @@
 import {Button} from '@/shared/shadcnComponent/ui/button';
 import Link from 'next/link';
 
-export default function Header() {
+export default async function Header() {
   return (
     <header className="flex justify-between items-center py-5 px-6 md:px-8 lg:py-6 lg:px-10">
       <Link href="/">
         <h2 className="text-2xl md:text-3xl font-bold text-boldBlue">모두의 : 후기</h2>
       </Link>
-      {/* Todo: 로그인 기능 구현하기 */}
-      <Button variant="logInOut" size="logInOut">
-        로그인
-      </Button>
+      <Link
+        href={`${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorize/kakao?redirect_uri=https://localhost:3000/oauth2/redirect`}
+      >
+        <Button variant="logInOut" size="logInOut">
+          로그인
+        </Button>
+      </Link>
     </header>
   );
 }
