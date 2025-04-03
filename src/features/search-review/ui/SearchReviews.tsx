002d910c0ff7c26c34c08e872c41d4262e640c27
queenJiJi <lovebellaberry52@gmail.com>
2025-04-04 01:58:16 +0900

    [feat] 로딩 및 에러 상태 처리 구현

--- src/features/search-review/ui/SearchReviews.tsx
+++ src/features/search-review/ui/SearchReviews.tsx
@@ -2,8 +2,10 @@
 
 import {CategoryBar, useSelectCategory} from '@/features/review-filtering';
 import SearchBar from '@/features/search/ui/SearchBar';
-import {Suspense} from 'react';
 import ReviewsWithScroll from './ReviewWithScroll';
+import {RQProvider} from '@/shared/providers';
+import {SearchReviewsLoading} from '@/features/search-review-keyword';
+import {LucideIcon} from '@/shared/ui/icons';
 
 export default function SearchReviews() {
   const {selectedCategory, handleSelectCategory} = useSelectCategory();
@@ -12,9 +14,12 @@ export default function SearchReviews() {
     <section>
       <CategoryBar selectedCategory={selectedCategory} onSelectCategory={handleSelectCategory} />
       <SearchBar />
-      <Suspense fallback={<div>Loading</div>}>
+      <RQProvider
+        LoadingFallback={<SearchReviewsLoading />}
+        icon={<LucideIcon name="Bug" className="w-28 h-28 md:w-40 md:h-40 mb-4 mt-12" />}
+      >
         <ReviewsWithScroll selectedCategory={selectedCategory} />
-      </Suspense>
+      </RQProvider>
     </section>
   );
 }
