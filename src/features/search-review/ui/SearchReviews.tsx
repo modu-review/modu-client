506aeaae6b31a65472dbd01e96212e4a4f3a54f9
queenJiJi <jiyeon.park@mail.mcgill.ca>
2025-04-04 00:16:21 +0900

    [feat] SearchReviews 컴포넌트 구현 -검색바표시, 카테고리 바 표시

--- src/features/search-review/ui/SearchReviews.tsx
+++ src/features/search-review/ui/SearchReviews.tsx
@@ -1,22 +1,15 @@
 'use client';
 
-import Pagination from '@/widgets/pagination';
-import {useSearchParams} from 'next/navigation';
+import {CategoryBar, useSelectCategory} from '@/features/review-filtering';
+import SearchBar from '@/features/search/ui/SearchBar';
 
-type Props = {
-  query: string;
-};
-
-export default function SearchReviews({query}: Props) {
-  const currentPage = Number(useSearchParams().get('page')) || 1;
-  // Todo: 검색어(query)를 기반으로 리뷰를 검색하는 API를 호출해 데이터를 불러온다. (페이지를 전달해 해당 페이지의 리뷰를 불러온다. 초기값 1)
-
-  // Todo: 페이지네이션을 구현한다. (useSearchParams를 통해 페이지를 전달받는다.)
+export default function SearchReviews() {
+  const {selectedCategory, handleSelectCategory} = useSelectCategory();
 
   return (
     <section>
-      {/* Todo: 검색 결과를 표시한다. */}
-      <Pagination />
+      <CategoryBar selectedCategory={selectedCategory} onSelectCategory={handleSelectCategory} />
+      <SearchBar />
     </section>
   );
 }
