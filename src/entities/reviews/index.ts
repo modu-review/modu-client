8e7fcd9d51536f29756be4a733c2076cc10f889f
queenJiJi <jiyeon.park@mail.mcgill.ca>
2025-04-04 00:29:37 +0900

    [feat] 무한스크롤 구현

--- src/entities/reviews/index.ts
+++ src/entities/reviews/index.ts
@@ -2,3 +2,5 @@ export * from './model/types';
 export {getBestReviews} from './apis/api-service';
 export {default as ReviewList} from './ui/ReviewList';
 export {default} from './model/useSearchReviewsWithQuery';
+export {default as ReviewArticle} from './ui/ReviewArticle';
+export {default as ReviewArticleLoading} from './ui/ReviewArticleLoading';
--- src/features/search-review/ui/ReviewWithScroll.tsx
+++ src/features/search-review/ui/ReviewWithScroll.tsx
@@ -1,4 +1,7 @@
+import {ReviewArticle, ReviewArticleLoading} from '@/entities/reviews';
 import useSearchReviews from '@/entities/reviews/model/useSearchReviews';
+
+import {useCallback} from 'react';
 
 type Props = {
   selectedCategory: string;
@@ -6,6 +9,37 @@ type Props = {
 
 export default function ReviewsWithScroll({selectedCategory}: Props) {
   const {data, hasNextPage, fetchNextPage, isFetchingNextPage} = useSearchReviews(selectedCategory);
-  console.log(data);
-  return <></>;
+
+  const observerRef = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (!node) return;
+
+      const observer = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+
+      observer.observe(node);
+
+      return () => observer.disconnect();
+    },
+    [hasNextPage, fetchNextPage],
+  );
+  return (
+    <>
+      <ul className="px-3 py-14">
+        {data.pages.map(page =>
+          page.results.map(review => (
+            <li key={review.board_id}>
+              <ReviewArticle searchReview={review} />
+            </li>
+          )),
+        )}
+      </ul>
+      <div className="h-[250px] w-full mt-6" ref={observerRef}>
+        {isFetchingNextPage && <ReviewArticleLoading />}
+      </div>
+    </>
+  );
 }
