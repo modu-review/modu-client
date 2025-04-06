fa14d011ac4ed3c7bb1ca41e43db5812343aa6bf
GoJiMin <gojimin3095@gmail.com>
2025-04-06 16:20:03 +0900

    [fix] 토큰 만료 API 요청 주소 변경 및 OAuth2 리다이렉트 페이지 export 네이밍 수정

--- src/views/oauth2/index.ts
+++ src/views/oauth2/index.ts
@@ -1,2 +1,2 @@
 export {default as OAuth2ErrorFallback} from './ui/OAuth2ErrorFallback';
-export {default as OAuth2FailPage} from './ui/OAuth2ErrorFallback';
+export {default as OAuth2RedirectPage} from './ui/OAuth2RedirectPage';
