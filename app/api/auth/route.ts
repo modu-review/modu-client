217dee587de7111e5abac84321164453bcb74fb1
GoJiMin <gojimin3095@gmail.com>
2025-04-07 22:11:24 +0900

    [feat] OAuth 인증 상태 확인을 위한 API 라우트 구현
    - accessToken 쿠키 존재 여부로 로그인 상태 확인.
    - UserEmail 쿠키에서 사용자 정보 추출.
    - 사용자 이메일에서 @의 앞부분을 아이디로 추출.
    - 200 응답과 함께 인증 상태 및 사용자 정보 반환.
    - 400 응답으로 이메일 정보 누락에 대한 예외 처리.
    
    해당 API는 어플리케이션이 실행되는 시점에 인증 세션을 zustand 스토어에 저장하기 위해 사용할 예정입니다.
    백엔드 서버로부터 전달된 HttpOnly 속성의 쿠키들로 사용자 세션을 서버에서 처리할 수 있어 인증이 필요한 라우팅에 대한 처리를
    보다 안전하게 처리할 수 있습니다.

---
+++ app/api/auth/route.ts
@@ -0,0 +1,30 @@
+import {cookies} from 'next/headers';
+import {NextResponse} from 'next/server';
+
+export async function GET() {
+  const cookieStore = await cookies();
+
+  if (!cookieStore.has('accessToken')) {
+    return NextResponse.json({
+      isLoggedIn: false,
+    });
+  }
+
+  const userEmailCookie = cookieStore.get('UserEmail');
+
+  if (!userEmailCookie) {
+    return NextResponse.json({message: 'EMPTY_USER_EMAIL'}, {status: 400});
+  }
+
+  const userEmail = userEmailCookie.value;
+  const userId = userEmail.split('@')[0];
+
+  return NextResponse.json(
+    {
+      isLoggedIn: true,
+      userEmail,
+      userId,
+    },
+    {status: 200},
+  );
+}
