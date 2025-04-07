265349528f5806796c5bf522694adf07adc43ec0
GoJiMin <gojimin3095@gmail.com>
2025-04-07 19:20:28 +0900

    [style] 일반 검색 페이지와의 UI 통일을 위한 스타일 수정

--- src/views/search/ui/SearchWithKeywordPage.tsx
+++ src/views/search/ui/SearchWithKeywordPage.tsx
@@ -1,5 +1,6 @@
 import SearchBar from '@/features/search';
 import {SearchReviewsWithKeyword} from '@/features/search-review-keyword';
+
 type Props = {
   params: Promise<{keyword: string}>;
 };
@@ -10,8 +11,8 @@ export default async function SearchWithKeywordPage({params}: Props) {
   const decodedQuery = decodeURIComponent(keyword);
 
   return (
-    <section className="w-full h-full md:max-w-5xl mx-auto p-5 px-6 mt-2">
-      <h2 className="text-2xl ml-5 mb-4 md:mb-9 font-semibold">{decodedQuery} 검색 결과</h2>
+    <section className="w-full h-full md:px-8 md:max-w-5xl mx-auto mt-2">
+      <h2 className="font-bold text-2xl mt-6 md:mt-10 mb-8 md:text-3xl px-5">{decodedQuery} 검색 결과</h2>
       <SearchBar />
       <SearchReviewsWithKeyword />
     </section>
