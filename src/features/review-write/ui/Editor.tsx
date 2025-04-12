15bd0b0690efc74789acb6c660613e172accbcd0
GoJiMin <gojimin3095@gmail.com>
2025-04-12 23:15:56 +0900

    [feat] titap 확장 기능 설치 (이미지, 정렬)
    - 이미지 및 정렬 기능을 추가로 사용하기 위해 확장 기능 설치.
    - 편집기 및 작성된 문서의 스타일 분리를 위한 타이포그라피 설정.

--- src/features/review-write/ui/Editor.tsx
+++ src/features/review-write/ui/Editor.tsx
@@ -2,12 +2,29 @@
 
 import {useEditor, EditorContent} from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import Image from '@tiptap/extension-image';
+import TextAlign from '@tiptap/extension-text-align';
 
 export default function Editor() {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: '<p>테스트</p>',
+    extensions: [
+      StarterKit,
+      Image.configure({
+        HTMLAttributes: {
+          class: 'custom-editor-image',
+        },
+      }),
+      TextAlign.configure({types: ['heading', 'paragraph']}),
+    ],
+    editorProps: {
+      scrollThreshold: 100,
+      scrollMargin: 100,
+      attributes: {
+        class: 'prose prose-p:my-3 prose-h1:font-bold lg:prose-lg focus:outline-none',
+      },
+    },
     immediatelyRender: false,
+    autofocus: true,
   });
 
   return <EditorContent editor={editor} />;
