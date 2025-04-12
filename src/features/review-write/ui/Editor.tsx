3507fb3c0992bfd240e63ea2bc71d017c55a0b70
GoJiMin <gojimin3095@gmail.com>
2025-04-13 01:56:01 +0900

    [feat] 제목 및 카테고리 입력 폼 구현
    EditorMetaForm.tsx
    - 제목과 카테고리 입력을 위한 입력 폼 구현.
    - 메인 에디터 컴포넌트 내 header 태그에 연결.
    - react-hook-form api를 통한 입력 값 검증 및 카테고리 선택 검증.
    - editor-meta-form id를 통한 submit 이벤트 통제.

--- src/features/review-write/ui/Editor.tsx
+++ src/features/review-write/ui/Editor.tsx
@@ -3,6 +3,8 @@
 import {EditorContent} from '@tiptap/react';
 import useReviewEditor from '../lib/useReviewEditor';
 import {Toolbar} from '../toolbar';
+import EditorMetaForm from './EditorMetaForm';
+import {Button} from '@/shared/shadcnComponent/ui/button';
 
 export default function Editor() {
   const {editor, editorRef} = useReviewEditor();
@@ -14,9 +16,16 @@ export default function Editor() {
   return (
     <section className="w-full h-[80vh] flex flex-col relative shadow-lg">
       <header>
+        <EditorMetaForm />
         <Toolbar editor={editor} />
       </header>
       <EditorContent ref={editorRef} spellCheck="false" className="p-5 h-full overflow-auto" editor={editor} />
+      <section className="w-full bg-white border-t border-gray-300 p-4 flex justify-end items-center gap-3">
+        <Button form="editor-meta-form" type="submit">
+          미리보기
+        </Button>
+        <Button>저장하기</Button>
+      </section>
     </section>
   );
 }
--- src/features/review-write/ui/EditorMetaForm.tsx
+++ src/features/review-write/ui/EditorMetaForm.tsx
@@ -1,3 +1,69 @@
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {FormSchema} from '../consts/rule';
+import {CATEGORY_LIST} from '../consts/categoryList';
+import {FormSchemaType} from '../model/type';
+import {Form, FormControl, FormField, FormItem, FormMessage} from '@/shared/shadcnComponent/ui/form';
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from '@/shared/shadcnComponent/ui/select';
+import {Input} from '@/shared/shadcnComponent/ui/input';
+
 export default function EditorMetaForm() {
-    return  
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      title: '',
+      category: undefined,
+    },
+  });
+
+  const onSubmit = (values: FormSchemaType) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form className="flex flex-col gap-4 mb-2 px-2" id="editor-meta-form" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="title"
+          render={({field}) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="text-xl py-2 border-none focus:outline-none"
+                  spellCheck="false"
+                  placeholder="제목을 입력하세요"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="category"
+          render={({field}) => (
+            <FormItem>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="w-[160px] focus:outline-none outline-none selection:outline-none">
+                    <SelectValue placeholder="카테고리" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {CATEGORY_LIST.map(({value, label}) => (
+                    <SelectItem key={value} value={value}>
+                      {label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 }
