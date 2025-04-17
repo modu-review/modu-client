700674179a197fa46edd46cd162cc6b54d2b74b9
GoJiMin <gojimin3095@gmail.com>
2025-04-17 18:01:40 +0900

    [feat] 로컬 이미지 삽입 기능 추가
    - 사용자가 로컬에서 선택한 이미지 파일을 object URL로 변환해 에디터 내 삽입.
    - custom-editor-image 스타일 정의.
      - 이미지 선택 표시를 위한 스타일링.
    
    이미지 업로드 기능은 다음과 같이 구현 예정입니다.
    1. 툴바의 이미지 버튼을 클릭한다.
    2. 사용자의 로컬 환경의 이미지를 선택한다.
    3. 이미지를 object URL로 변환해 웹 스토리지에 업로드하기 전 빠르게 에디터 내에 삽입한다.
    4. 백엔드 서버 측으로 presigned URL과 파일 id를 요청한다.
    5. 서버 측에서 받아온 presigned URL 경로에 받아온 파일 id로 업로드한다.
    6. S3 웹 스토리지와 연결된 cloudfront 도메인과 파일 id를 조합해 경로를 생성한다.
    7. 에디터 내 삽입된 img 태그의 src 속성을 6번에서 조합한 경로로 교체한다.
    
    이 흐름으로 구현하는 데 현재 1-3번에 해당하는 작업입니다.

--- src/features/review/editor/toolbar/ui/AddImage.tsx
+++ src/features/review/editor/toolbar/ui/AddImage.tsx
@@ -6,12 +6,24 @@ type Props = {
 };
 
 export default function AddImage({editor}: Props) {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    const files = event.target.files;
+    if (!files) return;
+
+    const file = files[0];
+    const tempUrl = URL.createObjectURL(file);
+
+    editor.chain().focus().setImage({src: tempUrl}).run();
+  };
+
   return (
     <label
       htmlFor="upload-image"
       className="w-[35px] h-[35px] flex items-center justify-center hover:bg-gray-100 cursor-pointer"
     >
-      <input id="upload-image" className="hidden" type="file" accept="image/*" />
+      <input id="upload-image" className="hidden" type="file" accept="image/*" onChange={handleChange} />
       <LucideIcon name="Images" className="w-[20px] h-[20px]" />
     </label>
   );
