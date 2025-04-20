9ff5a5180d95bb5faf2a9c06f7cec601033543ba
GoJiMin <gojimin3095@gmail.com>
2025-04-21 00:31:11 +0900

    [feat] 에디터 로딩 상태에서 null 반환 대신 로딩 스피너 표시

--- src/features/review/editor/ui/EditorContainer.tsx
+++ src/features/review/editor/ui/EditorContainer.tsx
@@ -3,6 +3,7 @@ import {useReviewEditor} from '../lib';
 import {Toolbar} from '../toolbar';
 import {EditorContent} from '@tiptap/react';
 import {EditorContentGetter} from '../model/type';
+import {LucideIcon} from '@/shared/ui/icons';
 
 type Props = {
   onMount: (getter: EditorContentGetter) => void;
@@ -20,7 +21,14 @@ export default function EditorContainer({onMount}: Props) {
     }));
   }, [editor, onMount]);
 
-  if (!editor || !editorState) return null;
+  if (!editor || !editorState) {
+    return (
+      <section className="w-full h-full flex flex-col justify-center items-center animate-pulse">
+        <h2 className="text-xl md:text-2xl font-semibold mb-4">에디터 정보를 불러오고 있어요.</h2>
+        <LucideIcon name="Disc3" className="animate-spin w-[40px] h-[40px] md:w-[45px] md:h-[45px]" />
+      </section>
+    );
+  }
 
   return (
     <>
