8934cbaf097739802a7a31594833d322a4dacb5b
GoJiMin <gojimin3095@gmail.com>
2025-05-21 17:39:31 +0900

    [feat] 페이지 제목 표시

--- src/views/mypage/ui/MyPage.tsx
+++ src/views/mypage/ui/MyPage.tsx
@@ -1,3 +1,9 @@
 export default function MyPage() {
-  return <section>마이페이지</section>;
+  return (
+    <section className="fixed inset-0 w-full h-full bg-white">
+      <section className="bg-lightBlue p-4">
+        <h2 className="text-2xl text-boldBlue font-bold">마이페이지</h2>
+      </section>
+    </section>
+  );
 }
