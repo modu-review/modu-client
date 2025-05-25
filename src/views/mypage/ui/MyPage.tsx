0fb61b2ce5fe36c7c048127f4b3e9824fceab630
GoJiMin <gojimin3095@gmail.com>
2025-05-26 00:04:47 +0900

    [feat] 내가 작성한 후기 및 저장한 후기를 보여줄 탭 리스트 구현
    - 상단의 내가 작성한 후기, 내가 저장한 후기 버튼을 클릭하면 해당 내용을 표시.

--- src/views/mypage/ui/MyPage.tsx
+++ src/views/mypage/ui/MyPage.tsx
@@ -1,15 +1,17 @@
 import checkSession from '@/shared/lib/utils/checkSession';
 import UserInfo from './UserInfo';
+import ReviewTabs from './ReviewTabs';
 
 export default async function MyPage() {
-  await checkSession();
+  // await checkSession();
 
   return (
-    <section className="fixed inset-0 w-full h-full bg-white">
-      <section className="bg-lightBlue p-4 flex flex-col justify-between">
+    <section className="fixed inset-0 w-full h-full bg-white overflow-auto">
+      <section className="bg-lightBlue p-4 flex flex-col justify-between mb-[120px]">
         <h2 className="text-2xl text-boldBlue font-bold">마이페이지</h2>
         <UserInfo />
       </section>
+      <ReviewTabs />
     </section>
   );
 }
---
+++ src/views/mypage/ui/ReviewTabs.tsx
@@ -0,0 +1,20 @@
+import {Tabs, TabsContent, TabsList, TabsTrigger} from '@/shared/shadcnComponent/ui/tabs';
+
+const tabsTriggerStyle = '';
+
+export default function ReviewTabs() {
+  return (
+    <Tabs defaultValue="my" className="w-full px-4">
+      <TabsList className="grid w-full grid-cols-2 gap-2">
+        <TabsTrigger className={tabsTriggerStyle} value="my">
+          내가 작성한 후기
+        </TabsTrigger>
+        <TabsTrigger className={tabsTriggerStyle} value="myBookmarks">
+          내가 저장한 후기
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="my">나는 이런 후기를 작성했어요.</TabsContent>
+      <TabsContent value="myBookmarks">나는 이런 후기를 저장했어요.</TabsContent>
+    </Tabs>
+  );
+}
