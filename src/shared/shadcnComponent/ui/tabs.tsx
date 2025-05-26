636f7ed6400f3a7db2f9d97d13b334fa1b9aa053
GoJiMin <gojimin3095@gmail.com>
2025-05-26 23:20:48 +0900

    [style] TabsTrigger 컴포넌트의 비활성 상태에 대한 hover 스타일 추가

--- src/shared/shadcnComponent/ui/tabs.tsx
+++ src/shared/shadcnComponent/ui/tabs.tsx
@@ -10,7 +10,7 @@ const Tabs = TabsPrimitive.Root;
 const tabsTriggerDefaultStyle =
   'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-base md:text-lg font-medium border-2 border-mediumBlue focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 transition-all';
 const tabsTriggerActiveStyle =
-  'data-[state=active]:pb-4 md:data-[state=active]:pb-3.5 data-[state=inactive]:mb-2.5 md:data-[state=inactive]:mb-2 data-[state=active]:bg-lightBlue data-[state=active]:border-lightBlue data-[state=active]:text-boldBlue';
+  'data-[state=active]:pb-4 md:data-[state=active]:pb-3.5 data-[state=inactive]:mb-2.5 md:data-[state=inactive]:mb-2 data-[state=active]:bg-lightBlue data-[state=active]:border-lightBlue data-[state=active]:text-boldBlue data-[state=inactive]:hover:bg-lightBlue data-[state=inactive]:hover:text-boldBlue data-[state=inactive]:hover:border-lightBlue data-[state=inactive]:hover:border-lightBlue';
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
