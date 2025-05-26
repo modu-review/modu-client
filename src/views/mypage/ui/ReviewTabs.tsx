eebfc4a2055fc02f2d58c9c11460cbd6a7132ac4
GoJiMin <gojimin3095@gmail.com>
2025-05-26 17:52:06 +0900

    [refactor] tab 분기 값을 URL 기반으로 변경

--- src/views/mypage/ui/ReviewTabs.tsx
+++ src/views/mypage/ui/ReviewTabs.tsx
@@ -1,18 +1,24 @@
+'use client';
+
+import {useRouter, useSearchParams} from 'next/navigation';
 import {MyBookmarkedReviews, MyReviews} from '@/features/reviews/my';
 import {Tabs, TabsContent, TabsList, TabsTrigger} from '@/shared/shadcnComponent/ui/tabs';
 
-const tabsTriggerStyle = '';
-
 export default function ReviewTabs() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const tabValue = searchParams.get('tab') || 'my';
+
+  const handleTabChange = (value: string) => {
+    router.push('/mypage?tab=' + value);
+  };
+
   return (
-    <Tabs defaultValue="my" className="w-full px-4">
+    <Tabs value={tabValue} onValueChange={handleTabChange} defaultValue="my" className="w-full px-4">
       <TabsList className="grid w-full grid-cols-2 gap-2">
-        <TabsTrigger className={tabsTriggerStyle} value="my">
-          내가 작성한 후기
-        </TabsTrigger>
-        <TabsTrigger className={tabsTriggerStyle} value="myBookmarks">
-          내가 저장한 후기
-        </TabsTrigger>
+        <TabsTrigger value="my">내가 작성한 후기</TabsTrigger>
+        <TabsTrigger value="myBookmarks">내가 저장한 후기</TabsTrigger>
       </TabsList>
       <TabsContent value="my">
         <MyReviews />
