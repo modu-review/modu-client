339f45434b1ed815e55bbcd9dd159ab04a19790b
GoJiMin <gojimin3095@gmail.com>
2025-06-02 21:35:26 +0900

    [remove] 이미지 리사이즈 익스텐션 제거
    - 모바일 환경에서 발생하는 터치 불가능 버그 및 이미지 크기가 무한정으로 커지는 버그 등으로 익스텐션 제거.

--- src/features/review/editor/lib/useReviewEditor.ts
+++ src/features/review/editor/lib/useReviewEditor.ts
@@ -4,7 +4,6 @@ import StarterKit from '@tiptap/starter-kit';
 import Image from '@tiptap/extension-image';
 import TextAlign from '@tiptap/extension-text-align';
 import Link from '@tiptap/extension-link';
-import ImageResize from 'tiptap-extension-resize-image';
 import {useUpdateGlobalError} from '@/entities/error';
 import {createClientError} from '@/shared/lib/utils/client-error';
 
@@ -55,7 +54,6 @@ function useReviewEditor(initialContent?: string) {
       }).extend({
         inclusive: false,
       }),
-      ImageResize,
     ],
     editorProps: {
       scrollThreshold: 100,
