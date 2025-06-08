5bd605953e339699bcdc7e934974cc13991b1610
GoJiMin <gojimin3095@gmail.com>
2025-06-08 21:05:41 +0900

    [feat] 프로젝트에서 사용할 카테고리 목록 최신화 및 중앙화를 위한 속성 설정
    - category.ts, categoryList.ts에서 사용 중인 카테고리 리스트를 중앙화하기 위해 id, value, label 필드를 가지도록 변경.

--- src/entities/review/consts/category.ts
+++ src/entities/review/consts/category.ts
@@ -1,13 +1,15 @@
 import {CategoryValue} from '../model/type';
 
 export const CATEGORY_LIST = [
-  {value: 'car', label: '자동차'},
-  {value: 'food', label: '음식'},
-  {value: 'company', label: '회사'},
-  {value: 'cosmetic', label: '화장품'},
-  {value: 'cafe', label: '카페'},
-  {value: 'devices1', label: '전자제품'},
-  {value: 'clothes', label: '의류'},
+  {id: 'all', value: 'all', label: '전체'},
+  {id: 'food', value: 'food', label: '음식'},
+  {id: 'car', value: 'car', label: '자동차'},
+  {id: 'cosmetic', value: 'cosmetic', label: '화장품'},
+  {id: 'clothes', value: 'clothes', label: '의류'},
+  {id: 'company', value: 'company', label: '회사'},
+  {id: 'device', value: 'device', label: '전자제품'},
+  {id: 'book', value: 'book', label: '책'},
+  {id: 'sports', value: 'sports', label: '운동용품'},
 ] as const;
 
 export const CATEGORY_MAP = CATEGORY_LIST.reduce(
