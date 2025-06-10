641f245c7c9dbf9615ec8e087d67dc4bbe87d600
GoJiMin <gojimin3095@gmail.com>
2025-06-10 18:52:40 +0900

    [fix] 동적 경로 매개변수 타입 변경

--- src/views/review/detail/ui/ReviewDetailPage.tsx
+++ src/views/review/detail/ui/ReviewDetailPage.tsx
@@ -2,13 +2,13 @@ import {getReviewDetail} from '@/entities/review';
 import {Viewer} from '@/features/review/viewer';
 
 type Props = {
-  params: Promise<{reviewId: number}>;
+  params: Promise<{reviewId: string}>;
 };
 
 export default async function ReviewDetailPage({params}: Props) {
   const {reviewId} = await params;
 
-  const {author, content, bookmarks, category, created_at, title} = await getReviewDetail(reviewId);
+  const {author, content, bookmarks, category, created_at, title} = await getReviewDetail(Number(reviewId));
   return (
     <section className="flex flex-col w-full max-w-5xl mx-auto">
       <Viewer title={title} author={author} content={content} category={category} created_at={created_at} />
