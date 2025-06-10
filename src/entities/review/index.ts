780e7c6a15da8c26790abeec4a14cc9980e0aa95
GoJiMin <gojimin3095@gmail.com>
2025-06-10 17:58:40 +0900

    [feat] 리뷰 상세보기 요청 및 데이터 시각화

--- src/entities/review/index.ts
+++ src/entities/review/index.ts
@@ -2,4 +2,5 @@ export * from './model/type';
 export * from './consts/category';
 export {usePostReview} from './model/usePostReview';
 export {getPresigned, uploadImage} from './apis/api-service';
+export {getReviewDetail} from './apis/api-service';
 export {CATEGORY_LIST} from './consts/category';
--- src/views/review/detail/ui/ReviewDetailPage.tsx
+++ src/views/review/detail/ui/ReviewDetailPage.tsx
@@ -1,12 +1,23 @@
+import {getReviewDetail} from '@/entities/review';
+
 type Props = {
-  params: Promise<{reviewId: string}>;
+  params: Promise<{reviewId: number}>;
 };
 
 export default async function ReviewDetailPage({params}: Props) {
   const {reviewId} = await params;
+
+  const {author, content, bookmarks, category, created_at, title} = await getReviewDetail(reviewId);
   return (
     <section>
-      <h2>{reviewId} 상세보기</h2>
+      <h2>
+        {reviewId} {title} 상세보기
+      </h2>
+      <p>작성자: {author}</p>
+      <p>카테고리: {category}</p>
+      <p>작성일: {new Date(created_at).toLocaleDateString()}</p>
+      <p>북마크 수: {bookmarks}</p>
+      <p>내용: {content}</p>
     </section>
   );
 }
