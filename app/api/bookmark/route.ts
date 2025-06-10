9ebb1ce428f171178de3841538bd7daaf8d06971
GoJiMin <gojimin3095@gmail.com>
2025-06-11 00:16:02 +0900

    [fix] 요청 페이로드에서 review_id를 board_id로 변경 및 북마크 데이터 처리 로직 추가

--- app/api/bookmark/route.ts
+++ app/api/bookmark/route.ts
@@ -1,7 +1,9 @@
+import {readFileSync, writeFileSync} from 'fs';
 import {NextRequest, NextResponse} from 'next/server';
+import path from 'path';
 
 export async function POST(req: NextRequest) {
-  const {user_id, review_id} = await req.json();
+  const {user_id, board_id} = await req.json();
 
   if (!user_id) {
     return NextResponse.json({
@@ -10,12 +12,24 @@ export async function POST(req: NextRequest) {
     });
   }
 
-  if (!review_id) {
+  if (!board_id) {
     return NextResponse.json({
       errorCode: 'REVIEW_ID_NOT_PROVIDED',
       message: '리뷰 ID가 제공되지 않았습니다.',
     });
   }
+
+  const filePath = path.join(process.cwd(), 'public/data', `review_bookmarks_${board_id}.json`);
+
+  const fileData = readFileSync(filePath, 'utf-8');
+  const bookmarksData = JSON.parse(fileData);
+
+  const newBookmarksData = {
+    hasBookmarked: !bookmarksData.hasBookmarked,
+    bookmarks: bookmarksData.hasBookmarked ? bookmarksData.bookmarks - 1 : bookmarksData.bookmarks + 1,
+  };
+
+  writeFileSync(filePath, JSON.stringify(newBookmarksData));
 
   return NextResponse.json({
     message: '북마크 성공',
