6d4209e41b6de1429c804bee6d202f240ba95713
GoJiMin <gojimin3095@gmail.com>
2025-06-13 00:25:00 +0900

    [feat] 댓글 영역 제목 및 댓글 수 표시

--- src/features/review/comments/ui/Comments.tsx
+++ src/features/review/comments/ui/Comments.tsx
@@ -1,7 +1,23 @@
+'use client';
+
+import {useGetReviewComments} from '@/entities/review';
+import {useSearchParams} from 'next/navigation';
+
 type Props = {
   reviewId: number;
 };
 
 export default function Comments({reviewId}: Props) {
-  return <section></section>;
+  const searchParams = useSearchParams();
+
+  const currentPage = Number(searchParams.get('page')) || 1;
+  const {comments, comments_count, total_pages} = useGetReviewComments(reviewId, currentPage);
+
+  return (
+    <section className="w-full px-6">
+      <h2 className="mr-auto text-xl border-b-2">
+        댓글쓰기 <span className="text-lg">{comments_count}</span>
+      </h2>
+    </section>
+  );
 }
