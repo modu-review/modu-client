bfaed7331135ef2c30be92d4877777a83e3b763a
GoJiMin <gojimin3095@gmail.com>
2025-06-12 20:09:26 +0900

    [feat] 북마크 컴포넌트 스켈레톤 UI 구현

--- src/views/review/detail/ui/ReviewDetailPage.tsx
+++ src/views/review/detail/ui/ReviewDetailPage.tsx
@@ -1,6 +1,6 @@
-import {getReviewDetail} from '@/entities/review';
-import {Bookmarks} from '@/features/review/bookmarks';
 import {Viewer} from '@/features/review/viewer';
+import {Bookmarks, BookmarksLoading} from '@/features/review/bookmarks';
+import {getReviewDetail} from '@/entities/review';
 import {RQProvider} from '@/shared/providers';
 
 type Props = {
@@ -15,7 +15,7 @@ export default async function ReviewDetailPage({params}: Props) {
   return (
     <section className="flex flex-col w-full max-w-5xl mx-auto items-center">
       <Viewer title={title} author={author} content={content} category={category} created_at={created_at} />
-      <RQProvider LoadingFallback={<div>loading...</div>}>
+      <RQProvider LoadingFallback={<BookmarksLoading />}>
         <Bookmarks reviewId={parsedReviewId} />
       </RQProvider>
     </section>
