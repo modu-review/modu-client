557210193bf76ffb60b8b734fdeaee441b3f4b60
GoJiMin <gojimin3095@gmail.com>
2025-06-14 00:00:13 +0900

    [feat] 화면 설계 변경으로 인해 UserInfo 컴포넌트 제거 및 Avatar 컴포넌트 구현

--- src/features/review/comments/ui/CommentsList.tsx
+++ src/features/review/comments/ui/CommentsList.tsx
@@ -1,5 +1,5 @@
 import {Comment} from '@/entities/review';
-import {UserInfo} from '@/shared/ui/components';
+import {Avatar} from '@/shared/ui/components';
 
 type Props = {
   comments: Comment[];
@@ -10,11 +10,22 @@ type Props = {
 export default function CommentsList({comments, currentPage, totalPages}: Props) {
   return (
     <>
-      <ul>
+      <ul className="flex flex-col gap-5 mt-6">
         {comments.map(({id, author, content, created_at, profile_image}) => (
           <li key={id}>
-            <UserInfo profileImage={profile_image} userId={author} />
-            <p>{content}</p>
+            <div className="flex gap-3 mx-2 px-3 pt-5 pb-8 mb-5 bg-slate-100 rounded-lg">
+              <Avatar src={profile_image} alt={`${author}님의 프로필 이미지`} />
+              <article>
+                <div className="flex items-center gap-2 mb-1">
+                  <span className="font-semibold ">{author}</span>
+                  <time className="text-sm text-gray-500" dateTime={created_at}>
+                    ({created_at})
+                  </time>
+                </div>
+                <p className="text-sm md:text-base">{content}</p>
+              </article>
+            </div>
+            <div className="w-full h-[1.5px] bg-gray-300" />
           </li>
         ))}
       </ul>
