2f1a7c841186fb46bd18bc6289ccbdf05d9c1fea
GoJiMin <gojimin3095@gmail.com>
2025-06-14 00:06:58 +0900

    [feat] 단일 댓글 표시를 담당하는 CommentCard 컴포넌트 구현

--- src/features/review/comments/ui/CommentsList.tsx
+++ src/features/review/comments/ui/CommentsList.tsx
@@ -1,5 +1,5 @@
+import CommentCard from './CommentCard';
 import {Comment} from '@/entities/review';
-import {Avatar} from '@/shared/ui/components';
 
 type Props = {
   comments: Comment[];
@@ -11,21 +11,9 @@ export default function CommentsList({comments, currentPage, totalPages}: Props)
   return (
     <>
       <ul className="flex flex-col gap-5 mt-6">
-        {comments.map(({id, author, content, created_at, profile_image}) => (
-          <li key={id}>
-            <div className="flex gap-3 mx-2 px-3 pt-5 pb-8 mb-5 bg-slate-100 rounded-lg">
-              <Avatar src={profile_image} alt={`${author}님의 프로필 이미지`} />
-              <article>
-                <div className="flex items-center gap-2 mb-1">
-                  <span className="font-semibold ">{author}</span>
-                  <time className="text-sm text-gray-500" dateTime={created_at}>
-                    ({created_at})
-                  </time>
-                </div>
-                <p className="text-sm md:text-base">{content}</p>
-              </article>
-            </div>
-            <div className="w-full h-[1.5px] bg-gray-300" />
+        {comments.map(comment => (
+          <li key={comment.id}>
+            <CommentCard {...comment} />
           </li>
         ))}
       </ul>
