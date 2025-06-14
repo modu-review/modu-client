a538e7316638b94db2e708a0d6932c7bd65e4001
GoJiMin <gojimin3095@gmail.com>
2025-06-15 02:23:35 +0900

    [style] 로그인 모달 레이아웃 및 스타일 적용

--- src/widgets/login-modal/ui/LoginModal.tsx
+++ src/widgets/login-modal/ui/LoginModal.tsx
@@ -1,3 +1,18 @@
 export default function LoginModal() {
-  return <section className="w-[400px] h-[300px] bg-white z-30"></section>;
+  return (
+    <section className="w-[300px] h-[130px] bg-white z-30 rounded-lg flex flex-col items-center">
+      <div className="flex-1 mt-5 text-center">
+        <h3 className="md:text-lg">로그인 후 이용 가능한 서비스입니다.</h3>
+        <p className="text-sm text-gray-500">로그인하면 더 많은 기능을 이용할 수 있어요.</p>
+      </div>
+      <div className="flex items-center w-full gap-1 px-3 mb-3">
+        <button className="w-full py-1.5 bg-boldBlue text-white rounded hover:bg-boldBlue/80 transition-colors">
+          로그인하기
+        </button>
+        <button className="w-full py-1.5 bg-gray-300 text-black rounded hover:bg-gray-300/80 transition-colors">
+          닫기
+        </button>
+      </div>
+    </section>
+  );
 }
