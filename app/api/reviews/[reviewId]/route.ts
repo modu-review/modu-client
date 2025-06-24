4caf92e9a93236f6c14d9551fc020428ab718935
GoJiMin <gojimin3095@gmail.com>
2025-06-25 01:25:18 +0900

    [feat] PATCH /api/reviews/[reviewId] 라우트 핸들러 구현
    - Mock API가 아닌, 실제 요청에 사용되는 라우트 핸들러 구현.
    - 리뷰 수정 요청 시 accessToken을 통한 사용자 세션 검증 후 백엔드로 PATCH 요청.
    - 성공 시 revalidateTag로 'review-[reviewId]' 태그에 해당하는 게시글 본문 영역의 데이터 캐시 무효화.
    - reviewId, 토큰 누락, 백엔드 측 오류 등에 대한 예외 처리 및 에러 응답 추가.
    
    공유사항
    - revalidateTag는 서버 단에서만 실행 가능한 메서드로 라우트 핸들러를 미들웨어 형식으로 사용하도록 구현했습니다.

--- app/api/reviews/[reviewId]/route.ts
+++ app/api/reviews/[reviewId]/route.ts
@@ -1,4 +1,6 @@
 import {readFileSync, writeFileSync} from 'fs';
+import {revalidateTag} from 'next/cache';
+import {cookies} from 'next/headers';
 import {NextRequest, NextResponse} from 'next/server';
 import path from 'path';
 
@@ -49,6 +51,7 @@ export async function PATCH(req: NextRequest, {params}: {params: Promise<{review
     const newReviewData = Object.assign(reviewData, updatedData);
 
     writeFileSync(filePath, JSON.stringify(newReviewData));
+    revalidateTag(`review-${reviewId}`);
 
     return NextResponse.json({message: '리뷰가 성공적으로 수정되었습니다.'});
   } catch (error) {
@@ -75,3 +78,62 @@ export async function PATCH(req: NextRequest, {params}: {params: Promise<{review
     );
   }
 }
+
+// TODO: 백엔드 API 구현 완료 후 주석 해제
+// export async function PATCH(req: NextRequest, {params}: {params: Promise<{reviewId: string}>}) {
+//   const {reviewId} = await params;
+
+//   if (!reviewId) {
+//     return NextResponse.json(
+//       {
+//         errorCode: 'REVIEW_ID_MISSING',
+//         message: '리뷰 ID가 제공되지 않았습니다.',
+//       },
+//       {
+//         status: 400,
+//       },
+//     );
+//   }
+
+//   const cookieStore = await cookies();
+//   const accessToken = cookieStore.get('accessToken');
+
+//   if (!accessToken) {
+//     return NextResponse.json(
+//       {
+//         errorCode: 'UNAUTHORIZED',
+//         message: '로그인이 필요합니다.',
+//       },
+//       {
+//         status: 401,
+//       },
+//     );
+//   }
+
+//   const reviewData = await req.json();
+
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/reviews/${reviewId}`, {
+//     method: 'PATCH',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Cookie: `accessToken=${accessToken.value}`,
+//     },
+//     body: JSON.stringify(reviewData),
+//   });
+
+//   if (!res.ok) {
+//     const errorData = await res.json();
+//     return NextResponse.json(
+//       {
+//         errorCode: errorData.errorCode || 'INTERNAL_SERVER_ERROR',
+//         message: errorData.message || '서버 오류가 발생했습니다.',
+//       },
+//       {
+//         status: res.status,
+//       },
+//     );
+//   }
+
+//   revalidateTag(`review-${reviewId}`);
+//   return NextResponse.json({status: 201});
+// }
