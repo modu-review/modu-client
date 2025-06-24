4d783599e95aadb11c0a487aeef90f0e38381ccf
GoJiMin <gojimin3095@gmail.com>
2025-06-25 00:49:48 +0900

    [feat] 게시글 수정 Mock API 구현
    - Object.assign을 통해 변경된 값 반영.

--- app/api/reviews/[reviewId]/route.ts
+++ app/api/reviews/[reviewId]/route.ts
@@ -1,4 +1,4 @@
-import {readFileSync} from 'fs';
+import {readFileSync, writeFileSync} from 'fs';
 import {NextRequest, NextResponse} from 'next/server';
 import path from 'path';
 
@@ -11,6 +11,46 @@ export async function GET(_: NextRequest, {params}: {params: Promise<{reviewId: 
     const fileData = readFileSync(filePath, 'utf-8');
 
     return NextResponse.json(JSON.parse(fileData));
+  } catch (error) {
+    if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
+      return NextResponse.json(
+        {
+          errorCode: 'REVIEW_NOT_FOOUND',
+          meesage: '리뷰를 찾을 수 없습니다.',
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+
+    return NextResponse.json(
+      {
+        errorCode: 'INTERNAL_SERVER_ERROR',
+        message: '서버 오류가 발생했습니다.',
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
+
+export async function PATCH(req: NextRequest, {params}: {params: Promise<{reviewId: string}>}) {
+  const {reviewId} = await params;
+  const filePath = path.join(process.cwd(), 'public/data', `review_detail_${reviewId}.json`);
+
+  try {
+    const fileData = readFileSync(filePath, 'utf-8');
+    const reviewData = JSON.parse(fileData);
+
+    const updatedData = await req.json();
+
+    const newReviewData = Object.assign(reviewData, updatedData);
+
+    writeFileSync(filePath, JSON.stringify(newReviewData));
+
+    return NextResponse.json({message: '리뷰가 성공적으로 수정되었습니다.'});
   } catch (error) {
     if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
       return NextResponse.json(
