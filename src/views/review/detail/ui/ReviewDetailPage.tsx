29e8ff7cd764797e0ee761b1ea2f81fdb9530966
GoJiMin <gojimin3095@gmail.com>
2025-06-25 14:56:07 +0900

    [feat] 게시글 상세보기 페이지 내 수정, 삭제 버튼 구현
    - getSessionUserEmail 함수를 통해 로그인 사용자의 email 값(sessionUserEmail) 반환.
    - sessionUserEmail과 게시글 본문 데이터의 author를 비교해 isAuthor(boolean) 할당.
    - isAuthor가 true일 때만 수정 및 삭제 버튼 렌더링.
    - 서버 컴포넌트에서 수행하기 때문에 조작 및 렌더링 시 깜빡임 방지.
    - 수정 버튼 클릭 시 reviewId에 해당하는 수정 페이지로 리다이렉트.

--- src/views/review/detail/ui/ReviewDetailPage.tsx
+++ src/views/review/detail/ui/ReviewDetailPage.tsx
@@ -1,6 +1,8 @@
 import ReviewDetailInteractive from './ReviewDetailInteractive';
 import {Viewer} from '@/features/review/viewer';
 import {getReviewDetail} from '@/entities/review';
+import getSessionUserEmail from '@/shared/lib/utils/getSessionUserEmail';
+import Link from 'next/link';
 
 type Props = {
   params: Promise<{reviewId: string}>;
@@ -12,8 +14,20 @@ export default async function ReviewDetailPage({params}: Props) {
 
   const {author, content, category, created_at, title} = await getReviewDetail(parsedReviewId);
 
+  const sessionUserEmail = await getSessionUserEmail();
+  const isAuthor = sessionUserEmail === author;
+
   return (
-    <section className="flex flex-col w-full max-w-5xl mx-auto items-center">
+    <section className="flex flex-col w-full max-w-5xl mx-auto items-center relative">
+      {isAuthor && (
+        <div className="absolute top-[17px] right-6 flex items-center gap-2">
+          <Link href={`/reviews/${reviewId}/edit`} className="text-gray-500 hover:text-gray-700">
+            수정
+          </Link>
+          {/* TODO: 삭제 버튼 클릭 시 게시글 삭제 요청 구현하기 */}
+          <button className="text-gray-500 hover:text-gray-700">삭제</button>
+        </div>
+      )}
       <Viewer title={title} author={author} content={content} category={category} created_at={created_at} />
       <ReviewDetailInteractive reviewId={parsedReviewId} category={category} />
     </section>
