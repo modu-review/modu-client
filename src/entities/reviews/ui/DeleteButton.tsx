a53a51e019f89643f5c914cdc6800b67963bfba1
GoJiMin <gojimin3095@gmail.com>
2025-06-28 01:59:12 +0900

    [feat] isPending 상태에 따라 로딩 오버레이 표시
    - isPending 상태에 따라 로딩 오버레이 표시.
    - 화면을 가득 채우는 오버레이 표시 및 isPending 상태에 따라 스크롤 락 구현.

--- src/entities/reviews/ui/DeleteButton.tsx
+++ src/entities/reviews/ui/DeleteButton.tsx
@@ -2,6 +2,8 @@
 
 import {Category} from '@/entities/review/model/type';
 import useDeleteReview from '@/entities/review/model/useDeleteReview';
+import {LoadingSpinner} from '@/shared/ui/components';
+import {useEffect} from 'react';
 
 type Props = {
   category: Category;
@@ -15,9 +17,26 @@ export default function DeleteButton({category, reviewId}: Props) {
     deleteReview({category, reviewId});
   };
 
+  useEffect(() => {
+    const prevHtmlOverflow = document.documentElement.style.overflow;
+
+    if (isPending) {
+      document.documentElement.style.overflow = 'hidden';
+    } else {
+      document.documentElement.style.overflow = prevHtmlOverflow;
+    }
+  }, [isPending]);
+
   return (
-    <button className="text-gray-500 hover:text-gray-700" onClick={handleDelete} disabled={isPending}>
-      {isPending ? '삭제 중' : '삭제'}
-    </button>
+    <>
+      <button className="text-gray-500 hover:text-gray-700" onClick={handleDelete} disabled={isPending}>
+        삭제
+      </button>
+      {isPending && (
+        <section className="fixed inset-0 z-50 bg-black/70">
+          <LoadingSpinner text="리뷰를 삭제 중이에요." />
+        </section>
+      )}
+    </>
   );
 }
