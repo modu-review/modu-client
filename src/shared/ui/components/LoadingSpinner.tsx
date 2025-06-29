5b4252bb5f15b9b613223289fc820ccfb63c904a
GoJiMin <gojimin3095@gmail.com>
2025-06-29 23:15:37 +0900

    [refactor] 로딩 스피너에 스타일 주입이 가능하도록 변경

--- src/shared/ui/components/LoadingSpinner.tsx
+++ src/shared/ui/components/LoadingSpinner.tsx
@@ -1,12 +1,14 @@
+import {cn} from '@/shared/lib/utils/cn';
 import {LucideIcon} from '../icons';
 
 type Props = {
   text?: string;
+  className?: HTMLDivElement['className'];
 };
 
-export default function LoadingSpinner({text}: Props) {
+export default function LoadingSpinner({text, className}: Props) {
   return (
-    <section className="w-full h-full flex flex-col justify-center items-center animate-pulse">
+    <section className={cn('w-full h-full flex flex-col justify-center items-center animate-pulse', className)}>
       {text && <p className="text-xl md:text-2xl font-semibold mb-4">{text}</p>}
       <LucideIcon name="Disc3" className="animate-spin w-[40px] h-[40px] md:w-[45px] md:h-[45px]" />
     </section>
