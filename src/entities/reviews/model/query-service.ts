7c83cae205cd2dd8f150c3ccb337d425bbd6a51c
GoJiMin <gojimin3095@gmail.com>
2025-07-01 20:28:14 +0900

    [fix] 쿼리 키 팩터리 구조 변경으로 옵션 객체의 참조 속성 변경

--- src/entities/reviews/model/query-service.ts
+++ src/entities/reviews/model/query-service.ts
@@ -1,11 +1,5 @@
 import {keepPreviousData} from '@tanstack/react-query';
-import {
-  getBestReviews,
-  getCategoryReviews,
-  getKeywordReviews,
-  getMyBookmarkedReviews,
-  getMyReviews,
-} from '../apis/api-service';
+import {getCategoryReviews, getKeywordReviews, getMyBookmarkedReviews, getMyReviews} from '../apis/api-service';
 
 export const reviewsQueryKeys = {
   all: () => ['reviews'] as const,
@@ -31,22 +25,22 @@ export const reviewsQueryKeys = {
 
 export const reviewsQueryOptions = {
   category: (categoryId: string, sort: string) => ({
-    queryKey: reviewsQueryKeys.category(categoryId, sort),
+    queryKey: reviewsQueryKeys.category.page(categoryId, sort),
     queryFn: ({pageParam}: {pageParam: number}) => getCategoryReviews(pageParam, categoryId, sort),
     initialPageParam: 0,
   }),
   keyword: (keyword: string, page: number, sort: string) => ({
-    queryKey: reviewsQueryKeys.keyword(keyword, page, sort),
+    queryKey: reviewsQueryKeys.keyword.page(keyword, page, sort),
     queryFn: () => getKeywordReviews(keyword, page, sort),
     placeholderData: keepPreviousData,
   }),
   my: (page: number) => ({
-    queryKey: reviewsQueryKeys.my(page),
+    queryKey: reviewsQueryKeys.my.page(page),
     queryFn: () => getMyReviews(page),
     placeholderData: keepPreviousData,
   }),
   myBookmarks: (page: number) => ({
-    queryKey: reviewsQueryKeys.myBookmarks(page),
+    queryKey: reviewsQueryKeys.myBookmarks.page(page),
     queryFn: () => getMyBookmarkedReviews(page),
     placeholderData: keepPreviousData,
   }),
