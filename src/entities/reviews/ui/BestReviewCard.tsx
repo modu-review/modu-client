41a6a78cd242f7083ff17fdef58c72b473de194b
GoJiMin <gojimin3095@gmail.com>
2025-07-02 15:12:02 +0900

    [refactor] 과도한 추상화 제거를 위한 CardFrame 분리
    - 복잡한 판별 유니온 타입 및 switch 문 제거.
    - 각 컴포넌트의 책임과 목적 명확화.
    - BestReviewcard: 베스트 리뷰 카드 전용 컴포넌트.
    - MyPageReviewCard: 마이페이지 리뷰 카드 전용 컴포넌트.

---
+++ src/entities/reviews/ui/BestReviewCard.tsx
@@ -0,0 +1,14 @@
+import CardDescription from './CardDescription';
+import {ReviewCard} from '@/entities/review/model/type';
+
+type Props = {
+  card: ReviewCard;
+};
+
+export default function BestReviewCard({card}: Props) {
+  return (
+    <section className="relative bg-white rounded-t-[115px] rounded-b-xl pt-7 w-[290px] h-[400px] md:w-[300px] md:h-[400px] lg:w-[280px] lg:h-[370px] shadow-lg shadow-black">
+      <CardDescription card={card} />
+    </section>
+  );
+}
---
+++ src/entities/reviews/ui/MyPageReviewCard.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import CardDescription from './CardDescription';
+import {ReviewCard} from '@/entities/review/model/type';
+import {LucideIcon} from '@/shared/ui/icons';
+
+type Props = {
+  card: ReviewCard;
+  isAuthor: boolean;
+  reviewId: string;
+  onDelete: () => void;
+};
+
+export default function MyPageReviewCard({card, isAuthor, reviewId, onDelete}: Props) {
+  return (
+    <section className="relative bg-white rounded-xl pt-4 w-[290px] h-[400px] md:w-[300px] md:h-[400px] lg:w-[260px] lg:h-[350px] shadow-md shadow-mediumBlue hover:translate-y-[-2px] hover:shadow-lg hover:shadow-boldBlue transition-all">
+      {isAuthor && (
+        <div className="absolute w-full px-4 pt-1 flex justify-between">
+          <Link href={`/reviews/${reviewId}/edit`} aria-label="리뷰 수정">
+            <LucideIcon name="PencilLine" size={20} />
+          </Link>
+          <button onClick={onDelete} aria-label="리뷰 삭제">
+            <LucideIcon name="X" size={20} />
+          </button>
+        </div>
+      )}
+      <CardDescription card={card} />
+    </section>
+  );
+}
