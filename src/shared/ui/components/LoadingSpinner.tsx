b61aaba4bc914cd661ff0da251b3fd3971ac2b14
GoJiMin <gojimin3095@gmail.com>
2025-07-03 17:49:55 +0900

    [refactor] 로딩스피너의 텍스트 및 아이콘 크기를 주입 받을 수 있도록 변경, 마이페이지에서 삭제 요청 시 로딩 오버레이 표시

--- src/shared/ui/components/LoadingSpinner.tsx
+++ src/shared/ui/components/LoadingSpinner.tsx
@@ -4,13 +4,20 @@ import {LucideIcon} from '../icons';
 type Props = {
   text?: string;
   className?: HTMLDivElement['className'];
+  textSize?: string;
+  iconSize?: string;
 };
 
-export default function LoadingSpinner({text, className}: Props) {
+export default function LoadingSpinner({
+  text,
+  className,
+  textSize = 'text-lg md:text-xl lg:text-2xl',
+  iconSize = 'w-[40px] h-[40px] md:w-[45px] md:h-[45px]',
+}: Props) {
   return (
     <section className={cn('w-full h-full flex flex-col justify-center items-center animate-pulse', className)}>
-      {text && <p className="text-xl md:text-2xl font-semibold mb-4">{text}</p>}
-      <LucideIcon name="Disc3" className="animate-spin w-[40px] h-[40px] md:w-[45px] md:h-[45px]" />
+      {text && <p className={cn(textSize, 'font-semibold mb-4')}>{text}</p>}
+      <LucideIcon name="Disc3" className={cn('animate-spin', iconSize)} />
     </section>
   );
 }
