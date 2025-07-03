b4f9d9b41b830d89436b8b6609d4f52ecef1e980
GoJiMin <gojimin3095@gmail.com>
2025-07-03 21:26:32 +0900

    [fix] 로그인 이후 하드 리프레시로 메인페이지 이동
    - router.push는 클라이언트 사이드 네비게이션으로 동작해 서버 단에서 HTML 파일을 새로 생성하지 않음.
    - Header 컴포넌트는 서버 단에서 화면을 그리는데 이 때 사용자의 refreshToken 쿠키 유무로 로그인 사용자를 판단함.
    - 로그인 이후 쿠키가 생성되어도 서버 단에서 화면을 다시 그리지 않기 때문에 로그인 이후 메인페이지로 이동했을 때 사용자는 다시 로그인 버튼을 보게 됨.
    
    - kakaoLogin 함수는 비동기 함수로 useEffect 내부에서 login 함수로 래핑.
    - 내부에서 await을 사용해 kakaoLogin 함수의 호출이 끝나길 기다린 후 window.location.replace를 호출해 메인 페이지로 하드 리프레시.
    - replace 메서드를 사용해 사용자의 뒤로가기를 방지.

--- src/features/oauth2/lib/useKakaoOAuthLogin.ts
+++ src/features/oauth2/lib/useKakaoOAuthLogin.ts
@@ -1,10 +1,9 @@
 import {createClientError} from '@/shared/lib/utils/client-error';
-import {useRouter, useSearchParams} from 'next/navigation';
+import {useSearchParams} from 'next/navigation';
 import {useEffect} from 'react';
 import kakaoLogin from '../apis/api-service';
 
 function useKakaoOAuthLogin() {
-  const router = useRouter();
   const searchParams = useSearchParams();
 
   useEffect(() => {
@@ -14,10 +13,14 @@ function useKakaoOAuthLogin() {
       throw createClientError('EMPTY_USER_EMAIL');
     }
 
-    kakaoLogin(email);
+    const login = async () => {
+      await kakaoLogin(email);
 
-    router.push('/');
-  }, [router, searchParams]);
+      window.location.replace('/');
+    };
+
+    login();
+  }, [searchParams]);
 }
 
 export default useKakaoOAuthLogin;
