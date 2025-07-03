022933eea580028518d696e26caab2fb5e9612bb
GoJiMin <gojimin3095@gmail.com>
2025-07-03 22:35:02 +0900

    [remove] 카테고리 탭 중 회사 탭 제거

--- src/entities/review/consts/category.ts
+++ src/entities/review/consts/category.ts
@@ -6,7 +6,6 @@ export const CATEGORY_LIST = [
   {id: 'car', value: 'car', label: '자동차'},
   {id: 'cosmetic', value: 'cosmetic', label: '화장품'},
   {id: 'clothes', value: 'clothes', label: '의류'},
-  {id: 'company', value: 'company', label: '회사'},
   {id: 'device', value: 'device', label: '전자제품'},
   {id: 'book', value: 'book', label: '책'},
   {id: 'sports', value: 'sports', label: '운동용품'},
