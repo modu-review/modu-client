f5d61d2f75b4463d8e99e064c09024a385de8cb7
GoJiMin <gojimin3095@gmail.com>
2025-07-03 22:13:38 +0900

    [fix] 댓글이 존재하지 않을 경우 CommentsList를 렌더링하지 않도록 변경

--- src/features/review/comments/ui/Comments.tsx
+++ src/features/review/comments/ui/Comments.tsx
@@ -17,13 +17,17 @@ export default function Comments({reviewId, category, openLoginModal}: Props) {
   const currentPage = Number(searchParams.get('page')) || 1;
   const {comments, comments_count, total_pages} = useGetReviewComments(reviewId, currentPage);
 
+  const isCommentsEmpty = comments.length === 0 && total_pages === 0;
+
   return (
     <section className="w-full px-2.5 md:px-6">
       <h2 className="mr-auto text-xl pb-1.5 border-b-2">
         댓글쓰기 <span className="text-lg">{comments_count}</span>
       </h2>
       <CommentsInput reviewId={reviewId} category={category} page={currentPage} openLoginModal={openLoginModal} />
-      <CommentsList comments={comments} currentPage={currentPage} totalPages={total_pages} reviewId={reviewId} />
+      {!isCommentsEmpty && (
+        <CommentsList comments={comments} currentPage={currentPage} totalPages={total_pages} reviewId={reviewId} />
+      )}
     </section>
   );
 }
