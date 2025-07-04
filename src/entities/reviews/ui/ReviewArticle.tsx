15f12652e7f0e0d3e34c9b460f9a8be836f1254d
GoJiMin <gojimin3095@gmail.com>
2025-07-04 14:53:16 +0900

    [fix] ReviewArticle 컴포넌트의 author 필드를 author_id로 변경

--- src/entities/reviews/ui/ReviewArticle.tsx
+++ src/entities/reviews/ui/ReviewArticle.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 export default function ReviewArticle({
-  searchReview: {author, board_id, bookmarks, category, comments_count, preview, created_at, image_url, title},
+  searchReview: {author_id, board_id, bookmarks, category, comments_count, preview, created_at, image_url, title},
 }: Props) {
   return (
     <article
@@ -20,7 +20,7 @@ export default function ReviewArticle({
           <p className="text-sm text-gray-500 mb-1">{created_at}</p>
           <div className="flex gap-3 text-sm text-gray-500">
             <p>{category}</p>
-            <p>{author}</p>
+            <p>{author_id}</p>
           </div>
         </article>
         <article className="px-4 md:px-0 text-sm line-clamp-2 md:line-clamp-3 mb-4 md:mb-7 md:flex-1 md:max-w-[400px] lg:max-w-[550px]">
