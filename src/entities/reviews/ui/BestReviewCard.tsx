12f00d23ec1a1d44609f757565960472273b6d82
GoJiMin <gojimin3095@gmail.com>
2025-07-05 21:20:46 +0900

    [feat] 베스트 리뷰 카드를 클릭하면 해당 게시글 페이지로 이동

--- src/entities/reviews/ui/BestReviewCard.tsx
+++ src/entities/reviews/ui/BestReviewCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import CardDescription from './CardDescription';
 import {ReviewCard} from '@/entities/review/model/type';
 
@@ -7,8 +8,10 @@ type Props = {
 
 export default function BestReviewCard({card}: Props) {
   return (
-    <section className="relative bg-white rounded-t-[115px] rounded-b-xl pt-7 w-[290px] h-[400px] md:w-[300px] md:h-[400px] lg:w-[280px] lg:h-[370px] shadow-lg shadow-black">
-      <CardDescription card={card} />
-    </section>
+    <Link href={`/reviews/${card.board_id}`}>
+      <article className="relative bg-white rounded-t-[115px] rounded-b-xl pt-7 w-[290px] h-[400px] md:w-[300px] md:h-[400px] lg:w-[280px] lg:h-[370px] shadow-lg shadow-black">
+        <CardDescription card={card} />
+      </article>
+    </Link>
   );
 }
