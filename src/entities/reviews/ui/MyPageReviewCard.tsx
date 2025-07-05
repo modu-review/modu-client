4131cd363933d303cfd27f423816de23c42d25eb
GoJiMin <gojimin3095@gmail.com>
2025-07-05 21:23:11 +0900

    [feat] 마이페이지 리뷰 카드를 클릭하면 해당 게시글 페이지로 이동

--- src/entities/reviews/ui/MyPageReviewCard.tsx
+++ src/entities/reviews/ui/MyPageReviewCard.tsx
@@ -22,30 +22,32 @@ export default function MyPageReviewCard({card, isAuthor, context}: Props) {
   };
 
   return (
-    <section className="relative bg-white rounded-xl pt-4 w-[290px] h-[400px] md:w-[300px] md:h-[400px] lg:w-[260px] lg:h-[350px] shadow-md shadow-mediumBlue hover:translate-y-[-2px] hover:shadow-lg hover:shadow-boldBlue transition-all">
-      {isPending && (
-        <div className="absolute inset-0 bg-black/60 rounded-xl z-50">
-          <LoadingSpinner text="리뷰를 삭제하고 있어요." className="text-white mt-3" textSize="text-xl" />
-        </div>
-      )}
-      {isAuthor && (
-        <div className="absolute w-full px-4 flex justify-between text-gray-500 transition-colors">
-          <Link className="hover:text-gray-800" href={`/reviews/${card.board_id}/edit`} aria-label="리뷰 수정">
-            <LucideIcon name="PencilLine" size={20} />
-          </Link>
-          <button
-            className="hover:text-gray-800"
-            onClick={handleDelete}
-            disabled={isPending}
-            tabIndex={isPending ? -1 : 0}
-            aria-label="리뷰 삭제"
-            aria-disabled={isPending}
-          >
-            <LucideIcon name="X" size={20} />
-          </button>
-        </div>
-      )}
-      <CardDescription card={card} />
-    </section>
+    <Link href={`/reviews/${card.board_id}`}>
+      <article className="relative bg-white rounded-xl pt-4 w-[290px] h-[400px] md:w-[300px] md:h-[400px] lg:w-[260px] lg:h-[350px] shadow-md shadow-mediumBlue hover:translate-y-[-2px] hover:shadow-lg hover:shadow-boldBlue transition-all">
+        {isPending && (
+          <div className="absolute inset-0 bg-black/60 rounded-xl z-50">
+            <LoadingSpinner text="리뷰를 삭제하고 있어요." className="text-white mt-3" textSize="text-xl" />
+          </div>
+        )}
+        {isAuthor && (
+          <div className="absolute w-full px-4 flex justify-between text-gray-500 transition-colors">
+            <Link className="hover:text-gray-800" href={`/reviews/${card.board_id}/edit`} aria-label="리뷰 수정">
+              <LucideIcon name="PencilLine" size={20} />
+            </Link>
+            <button
+              className="hover:text-gray-800"
+              onClick={handleDelete}
+              disabled={isPending}
+              tabIndex={isPending ? -1 : 0}
+              aria-label="리뷰 삭제"
+              aria-disabled={isPending}
+            >
+              <LucideIcon name="X" size={20} />
+            </button>
+          </div>
+        )}
+        <CardDescription card={card} />
+      </article>
+    </Link>
   );
 }
