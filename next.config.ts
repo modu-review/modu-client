8c7306dae431a6529e08d8f06ea5a86923b08573
GoJiMin <gojimin3095@gmail.com>
2025-07-05 21:17:19 +0900

    [feat] Image 컴포넌트의 허용 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {remotePatterns: [{protocol: 'https', hostname: 'picsum.photos'}]},
+  images: {
+    remotePatterns: [
+      {protocol: 'https', hostname: 'picsum.photos'},
+      {protocol: 'https', hostname: 'd1izijuzr22yly.cloudfront.net'},
+    ],
+  },
 };
-
 export default nextConfig;
