fea87c73ab04b324cfd427315557ba02d45439da
GoJiMin <gojimin3095@gmail.com>
2025-07-08 21:19:40 +0900

    [style] BestReviews 컴포넌트 레이아웃 변경

--- src/features/reviews/best/ui/BestReviews.tsx
+++ src/features/reviews/best/ui/BestReviews.tsx
@@ -15,17 +15,19 @@ export default function BestReviews({reviews}: Props) {
   const filteredReview = reviews[selectedCategory];
 
   return (
-    <section className=" flex flex-col items-center justify-center bg-boldBlue mt-16 py-12 md:px-8">
-      <h4 className="text-white font-bold text-2xl mb-8 md:text-3xl">🔥 BEST 후기 🔥</h4>
-      <CategoryBar selectedCategory={selectedCategory} onSelectCategory={handleSelectCategory} />
-      <ReviewList filteredReview={filteredReview} />
-      <Link
-        href="/search"
-        className="flex items-center animate-bounce cursor-pointer text-white font-bold text-[20px]"
-        aria-label="더 많은 후기 보러가기"
-      >
-        더 많은 후기 보기 <LucideIcon name="ArrowRight" size={20} />
-      </Link>
+    <section className="bg-boldBlue mt-16">
+      <div className="w-full max-w-[1400px] mx-auto flex flex-col items-center justify-center py-12 md:px-8">
+        <h4 className="text-white font-bold text-2xl mb-8 md:text-3xl">🔥 BEST 후기 🔥</h4>
+        <CategoryBar selectedCategory={selectedCategory} onSelectCategory={handleSelectCategory} />
+        <ReviewList filteredReview={filteredReview} />
+        <Link
+          href="/search"
+          className="flex items-center animate-bounce cursor-pointer text-white font-bold text-[20px]"
+          aria-label="더 많은 후기 보러가기"
+        >
+          더 많은 후기 보기 <LucideIcon name="ArrowRight" size={20} />
+        </Link>
+      </div>
     </section>
   );
 }
