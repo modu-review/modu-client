2b73dac9941ba7418e6ecc4d7122baf804cf6c73
GoJiMin <gojimin3095@gmail.com>
2025-07-10 22:33:05 +0900

    [style] 검색 페이지 제목 위아래 여백 수정

--- src/views/search/ui/CategorySearchPage.tsx
+++ src/views/search/ui/CategorySearchPage.tsx
@@ -3,7 +3,7 @@ import {CategoryReviews} from '@/features/reviews/category';
 export default function CategorySearchPage() {
   return (
     <section className="flex flex-col md:px-8 max-w-5xl mx-auto">
-      <h4 className="font-bold text-2xl mt-6 md:mt-10 mb-8 md:text-3xl px-5"> 후기글 모음 </h4>
+      <h4 className="font-bold text-2xl mt-3 mb-4 md:mb-6 md:text-3xl px-5"> 후기글 모음 </h4>
       <CategoryReviews />
     </section>
   );
--- src/views/search/ui/KeywordSearchPage.tsx
+++ src/views/search/ui/KeywordSearchPage.tsx
@@ -12,7 +12,7 @@ export default async function KeywordSearchPage({params}: Props) {
 
   return (
     <section className="w-full h-full md:px-8 md:max-w-5xl mx-auto mt-2">
-      <h2 className="font-bold text-2xl mt-6 md:mt-10 mb-8 md:text-3xl px-5">{decodedQuery} 검색 결과</h2>
+      <h2 className="font-bold text-2xl mt-3 mb-6 md:mb-8 md:text-3xl px-5">{decodedQuery} 검색 결과</h2>
       <SearchBar />
       <KeywordReviews />
     </section>
