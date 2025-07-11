eb1988478c43a78cb0d89c32fa4b2bbf28f66329
GoJiMin <gojimin3095@gmail.com>
2025-07-12 00:53:44 +0900

    [fix] useCategoryReviews 훅의 인자로 Category 타입을 받도록 수정

--- src/entities/reviews/model/useCategoryReviews.ts
+++ src/entities/reviews/model/useCategoryReviews.ts
@@ -1,7 +1,8 @@
 import {useSuspenseInfiniteQuery} from '@tanstack/react-query';
 import {reviewsQueryOptions} from './query-service';
+import {Category} from '@/entities/review';
 
-export default function useCategoryReviews(categoryId: string, sort: string) {
+export default function useCategoryReviews(categoryId: Category, sort: string) {
   return useSuspenseInfiniteQuery({
     ...reviewsQueryOptions.category(categoryId, sort),
     getNextPageParam: lastPage => {
