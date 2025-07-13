7cd0995504b67bda547e9ecf1fb0d3cfdea1769a
GoJiMin <gojimin3095@gmail.com>
2025-07-13 23:19:04 +0900

    [feat] FloatingWriteButton 컴포넌트에 로그인 상태에 따른 모달 처리 추가

--- src/views/search/ui/FloatingWriteButton.tsx
+++ src/views/search/ui/FloatingWriteButton.tsx
@@ -1,9 +1,39 @@
+'use client';
+
+import {useRouter} from 'next/navigation';
+import {useIsLoggedIn} from '@/entities/auth';
 import {LucideIcon} from '@/shared/ui/icons';
+import {LoginModal, Modal, useModal} from '@/shared/ui/modal';
 
 export default function FloatingWriteButton() {
+  const router = useRouter();
+
+  const isLoggedIn = useIsLoggedIn();
+  const {openModal, handleModalOpen, handleModalClose} = useModal();
+
+  const handleClick = () => {
+    if (!isLoggedIn) {
+      handleModalOpen();
+      return;
+    }
+
+    router.push('/reviews/new', {scroll: false});
+  };
+
   return (
-    <button className="fixed bottom-4 right-4 md:bottom-8 md:right-6 lg:right-8 bg-boldBlue text-white rounded-full p-3 shadow-lg hover:bg-extraboldBlue transition-colors">
-      <LucideIcon name="Plus" size={24} />
-    </button>
+    <>
+      <button
+        className="fixed bottom-4 right-4 md:bottom-8 md:right-6 lg:right-8 bg-boldBlue text-white rounded-full p-3 shadow-lg hover:bg-extraboldBlue transition-colors"
+        onClick={handleClick}
+        aria-label="리뷰 작성하기"
+      >
+        <LucideIcon name="Plus" size={24} />
+      </button>
+      {openModal && (
+        <Modal onClose={handleModalClose}>
+          <LoginModal onClose={handleModalClose} />
+        </Modal>
+      )}
+    </>
   );
 }
