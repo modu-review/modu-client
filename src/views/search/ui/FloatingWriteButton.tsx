88df6ad6db1e2f6601c7826e24ee90cf355ce2b6
GoJiMin <gojimin3095@gmail.com>
2025-07-13 23:10:19 +0900

    [feat] FloatingWriteButton 컴포넌트 구현
    - 검색 페이지에서 게시글 작성 페이지로 라우팅하기 위한 플로팅 버튼 컴포넌트 구현.

---
+++ src/views/search/ui/FloatingWriteButton.tsx
@@ -0,0 +1,9 @@
+import {LucideIcon} from '@/shared/ui/icons';
+
+export default function FloatingWriteButton() {
+  return (
+    <button>
+      <LucideIcon name="Plus" size={24} />
+    </button>
+  );
+}
