fafd0814fed3b48e513a0806d7707043ea2b9b04
GoJiMin <gojimin3095@gmail.com>
2025-07-13 23:36:20 +0900

    [style] 로그인 모달 팝업 애니메이션 적용

--- src/shared/ui/modal/ui/LoginModal.tsx
+++ src/shared/ui/modal/ui/LoginModal.tsx
@@ -12,7 +12,7 @@ export default function LoginModal({onClose}: Props) {
   }
 
   return (
-    <section className="w-[350px] md:w-[400px] h-[200px] md:h-[250px] bg-white z-30 rounded-lg flex flex-col gap-10 items-center">
+    <section className="w-[350px] md:w-[400px] h-[200px] md:h-[250px] bg-white z-30 rounded-lg flex flex-col gap-10 items-center animate-fade-up">
       <div className="flex-1 text-center mt-12 md:mt-20">
         <h3 className="text-lg md:text-2xl font-bold mb-2">로그인 후 이용 가능한 서비스입니다.</h3>
         <p className="text-sm md:text-base text-gray-500">로그인하면 더 많은 기능을 이용할 수 있어요.</p>
