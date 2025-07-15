6a26d031ef436ab24066f39172b5a18af68ea35e
GoJiMin <gojimin3095@gmail.com>
2025-07-16 00:52:40 +0900

    [style] 모바일 환경의 리뷰 카드 넓이 수정

--- src/entities/reviews/ui/BestReviewCard.tsx
+++ src/entities/reviews/ui/BestReviewCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function BestReviewCard({card}: Props) {
   return (
     <Link href={`/reviews/${card.board_id}`}>
-      <article className="relative bg-white rounded-t-[115px] rounded-b-xl pt-7 w-[290px] h-[400px] md:w-[300px] md:h-[400px] lg:w-[280px] lg:h-[370px] shadow-lg shadow-gray-800 hover:translate-y-[-2px] hover:shadow-black hover:shadow-lg transition-all">
+      <article className="relative bg-white rounded-t-[115px] rounded-b-xl pt-7 w-[310px] h-[400px] md:w-[300px] md:h-[400px] lg:w-[280px] lg:h-[370px] shadow-lg shadow-gray-800 hover:translate-y-[-2px] hover:shadow-black hover:shadow-lg transition-all">
         <CardDescription card={card} />
       </article>
     </Link>
--- src/entities/reviews/ui/CardLoading.tsx
+++ src/entities/reviews/ui/CardLoading.tsx
@@ -2,7 +2,7 @@ import {Skeleton} from '@/shared/shadcnComponent/ui/skeleton';
 
 export default function CardLoading() {
   return (
-    <section className="flex flex-col items-center rounded-xl pt-4 w-[290px] h-[400px] md:w-[300px] md:h-[400px] lg:w-[260px] lg:h-[350px] bg-white shadow-lg">
+    <section className="flex flex-col items-center rounded-xl pt-4 w-[310px] h-[400px] md:w-[300px] md:h-[400px] lg:w-[260px] lg:h-[350px] bg-white shadow-lg">
       <div className="w-full px-3 flex justify-between items-center">
         <Skeleton className="w-[30px] h-[20px] mb-3" />
         <Skeleton className="w-[100px] h-[25px] mb-3" />
