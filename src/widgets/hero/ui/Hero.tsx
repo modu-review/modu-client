9e8fc6c6787800d702fda2529d9cb6b0a9081c91
GoJiMin <gojimin3095@gmail.com>
2025-07-16 01:23:48 +0900

    [style] 모바일 환경에서 슬로건 가독성 개선
    - 첫 번째 문장의 자연스러운 줄바꿈 지점을 조정 (못한 것을 => 못한 것을 누군가).

--- src/widgets/hero/ui/Hero.tsx
+++ src/widgets/hero/ui/Hero.tsx
@@ -4,10 +4,9 @@ import {SearchBar} from '@/features/reviews/search-bar';
 export default function Hero() {
   return (
     <section className="flex flex-col justify-center items-center mt-20">
-      <article className="text-center font-semibold mb-8 text-[18px] md:text-[26px] lg:text-[32px]">
-        <p>
-          내가 경험하지 못한 것을
-          <br className="md:hidden" /> 누군가는 먼저 경험했습니다.
+      <article className="text-center font-semibold mb-8 text-[20px] md:text-[26px] lg:text-[32px]">
+        <p className="mb-3 md:mb-0">
+          내가 경험하지 못한 것을 누군가는 <br className="md:hidden" /> 먼저 경험했습니다.
         </p>
         <p>세상의 모든 후기를</p>
         <p>
