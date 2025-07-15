c953b2b2cb3ab2f77190cfa1bc5cca4996dad3a8
GoJiMin <gojimin3095@gmail.com>
2025-07-15 22:25:40 +0900

    [style] CardDescription 컴포넌트 스타일 수정

--- src/entities/reviews/ui/CardDescription.tsx
+++ src/entities/reviews/ui/CardDescription.tsx
@@ -13,8 +13,8 @@ export default function CardDescription({
     <article className="h-full flex flex-col items-center justify-between font-semibold">
       <p className="text-sm">{CATEGORY_MAP[category]}</p>
 
-      <div className="flex flex-col gap-1 items-center text-center">
-        <div className="w-[140px] h-[140px] overflow-hidden rounded-[30px] mb-3 shadow-md shadow-black">
+      <div className="flex flex-col items-center text-center">
+        <div className="w-[140px] h-[140px] overflow-hidden rounded-[30px] mb-1 shadow-md shadow-black">
           <Image
             className="w-full h-full object-cover aspect-square"
             src={image_url}
@@ -24,9 +24,9 @@ export default function CardDescription({
             alt={`카드 이미지: ${title}`}
           />
         </div>
-        <h3 className="mt-2">{title}</h3>
+        <h3 className="mt-4 mb-2 px-3 line-clamp-2">{title}에 추가로 뭔가 더 길어진다면?</h3>
         <div className="min-h-[30px]">
-          <p className="text-[12px] font-light line-clamp-2 px-5">{preview}</p>
+          <p className="text-[13px] font-light line-clamp-2 md:line-clamp-2 px-5">{preview}</p>
         </div>
       </div>
 
