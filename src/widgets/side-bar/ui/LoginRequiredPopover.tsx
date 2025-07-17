fd41be516bfebc377162e35cc930e783d4518281
GoJiMin <gojimin3095@gmail.com>
2025-07-17 23:41:17 +0900

    [style] LoginRequiredPopover 컴포넌트 스타일링

--- src/widgets/side-bar/ui/LoginRequiredPopover.tsx
+++ src/widgets/side-bar/ui/LoginRequiredPopover.tsx
@@ -16,15 +16,20 @@ export default function LoginRequiredPopover({title}: Props) {
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <button>
+        <button className="flex items-center text-muted-foreground gap-1 hover:text-boldBlue transition-colors">
           <LucideIcon name="LockKeyhole" size={20} /> <span>{title}</span>
         </button>
       </PopoverTrigger>
       <PopoverContent>
-        <div>
-          <h3>로그인 후 이용할 수 있어요.</h3>
-          <p>로그인하면 더 많은 기능을 이용할 수 있어요.</p>
-          <Link href={LOGIN_URL}>로그인하기</Link>
+        <div className="flex flex-col">
+          <h3 className="font-semibold mb-2">로그인 후 이용할 수 있어요.</h3>
+          <p className="text-sm text-muted-foreground mb-1">로그인하면 더 많은 기능을 이용할 수 있어요.</p>
+          <Link
+            href={LOGIN_URL}
+            className="w-full text-center text-sm bg-boldBlue text-white py-1.5 font-semibold rounded-md hover:bg-extraboldBlue transition-colors"
+          >
+            로그인하기
+          </Link>
         </div>
       </PopoverContent>
     </Popover>
