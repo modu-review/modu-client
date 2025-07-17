30388180b03badf8b24593fc33f98d5dc177965a
GoJiMin <gojimin3095@gmail.com>
2025-07-17 23:25:32 +0900

    [feat] 헤더의 로그인 버튼 영역을 사이드바로 변경

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,4 +1,4 @@
-import {Button} from '@/shared/shadcnComponent/ui/button';
+import {Sidebar} from '@/widgets/side-bar';
 import {cookies} from 'next/headers';
 import Link from 'next/link';
 
@@ -6,30 +6,13 @@ export default async function Header() {
   const cookieStore = await cookies();
 
   const isLoggedIn = cookieStore.has('refreshToken');
-  const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL;
-
-  if (!LOGIN_URL) {
-    throw new Error('로그인 URL이 환경변수에 정의되지 않았습니다.');
-  }
 
   return (
     <header className="flex justify-between items-center py-5 px-6 md:px-8 lg:py-6 lg:px-10">
       <Link href="/">
         <h2 className="text-2xl md:text-3xl font-bold text-boldBlue">모두의 : 후기</h2>
       </Link>
-      {isLoggedIn ? (
-        <Link href={'/mypage'} scroll={false}>
-          <Button variant="logInOut" size="logInOut">
-            마이페이지
-          </Button>
-        </Link>
-      ) : (
-        <Link href={`${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/kakao`}>
-          <Button variant="logInOut" size="logInOut">
-            로그인
-          </Button>
-        </Link>
-      )}
+      <Sidebar />
     </header>
   );
 }
