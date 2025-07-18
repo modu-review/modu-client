6e35044bf9dbd55d2cb5b1713cbbfb93e0d44317
GoJiMin <gojimin3095@gmail.com>
2025-07-19 01:05:07 +0900

    [feat] LoginRequiredPopOver 컴포넌트의 Link 컴포넌트를 LoginButton으로 변경 및 LoginButton이 className과 text를 받을 수 있도록 변경

--- src/widgets/side-bar/ui/LoginButton.tsx
+++ src/widgets/side-bar/ui/LoginButton.tsx
@@ -1,7 +1,12 @@
 import {useGoLoginPage} from '@/entities/auth';
 import {useGetFullPathName} from '@/shared/hooks';
 
-export default function LoginButton() {
+type Props = {
+  text?: string;
+  className?: HTMLButtonElement['className'];
+};
+
+export default function LoginButton({text = '로그인', className}: Props) {
   const fullPath = useGetFullPathName();
   const {goLoginPage} = useGoLoginPage();
 
@@ -13,10 +18,10 @@ export default function LoginButton() {
     <button
       type="button"
       onClick={handleClickLoginButton}
-      aria-label="로그인"
-      className="w-full text-center bg-boldBlue text-white py-1.5 font-semibold rounded-md hover:bg-extraboldBlue transition-colors mb-3"
+      aria-label={text}
+      className={`w-full text-center bg-boldBlue text-white py-1.5 font-semibold rounded-md hover:bg-extraboldBlue transition-colors ${className}`}
     >
-      로그인
+      {text}
     </button>
   );
 }
