138a9bb659ef6f2680935629bd29dcdb89d7c6b0
GoJiMin <gojimin3095@gmail.com>
2025-07-18 22:38:43 +0900

    [feat] authConstants, useGoLoginPage 엔트리 파일 연결

--- src/entities/auth/index.ts
+++ src/entities/auth/index.ts
@@ -1,4 +1,6 @@
 export * from './model/types';
+export * from './consts/authConstants';
 export {useIsLoggedIn, useUserEmail, useUserId, useUpdateUser} from './model/authStore';
 export {getSession} from './apis/api-service';
 export {logout} from './apis/api-service';
+export {default as useGoLoginPage} from './model/useGoLoginPage';
