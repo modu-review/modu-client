9840fcef1036415def70ad27e56d3c868991f14e
GoJiMin <gojimin3095@gmail.com>
2025-07-19 13:27:30 +0900

    [refactor] LoginModal을 features 레이어의 auth 슬라이스로 이동
    - 향후 로그인 버튼 통합 사용 시 shared => features 참조 불가 문제 해결을 위해 features 레이어로 이동

--- src/shared/ui/modal/index.ts
+++ src/shared/ui/modal/index.ts
@@ -2,4 +2,3 @@ export {default as Modal} from './ui/Modal';
 export {useModal} from './lib/useModal';
 export {default as DeleteConfirmModal} from './ui/DeleteConfirmModal';
 export {default as ConfirmDeleteTrigger} from './ui/ConfirmDeleteTrigger';
-export {default as LoginModal} from './ui/LoginModal';
