c82412ce24f7c92ffb4a7b943ebfebb492834192
GoJiMin <gojimin3095@gmail.com>
2025-07-20 23:44:27 +0900

    [style] 검색 페이지의 margin-top, left 수정

--- src/views/search/ui/CategorySearchPage.tsx
+++ src/views/search/ui/CategorySearchPage.tsx
@@ -4,7 +4,7 @@ import FloatingWriteButton from './FloatingWriteButton';
 export default function CategorySearchPage() {
   return (
     <section className="flex flex-col md:px-8 max-w-5xl mx-auto">
-      <h4 className="font-bold text-2xl mt-3 mb-4 md:mb-6 md:text-3xl px-5"> 후기글 모음 </h4>
+      <h4 className="font-bold text-2xl mt-3 md:mt-6 mb-4 md:mb-6 md:text-3xl ml-7"> 후기글 모음 </h4>
       <CategoryReviews />
       <FloatingWriteButton />
     </section>
