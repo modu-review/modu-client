95eef88f1c2bd991cbc1fc36f8fe2114c54dae2d
GoJiMin <gojimin3095@gmail.com>
2025-07-20 21:49:00 +0900

    [feat] 리뷰 카드 렌더링 시 우선순위 설정 기능 추가
    - Next.js의 이미지 컴포넌트는 기본적으로 lazy 로딩.
    - priority 속성을 true로 사용 시 우선 로딩이 가능한데, 현재 CardDescription 컴포넌트의 priority 값은 항상 true인 상태로 상위 3개 카드에 대해서만 우선 로딩하기 위해 조건부 boolean 값을 전달.

--- src/entities/reviews/ui/ReviewsGrid.tsx
+++ src/entities/reviews/ui/ReviewsGrid.tsx
@@ -34,20 +34,34 @@ export default function ReviewsGrid(props: Props) {
   const {reviews, from} = props;
   const variant = from === 'bestReviews' ? 'bestReviews' : 'myPage';
 
-  function renderCardFrame(card: ReviewCard) {
+  function renderCardFrame(card: ReviewCard, index: number) {
+    const shouldPrioritize = index < 3;
+
     switch (from) {
       case 'myReviews':
-        return <MyPageReviewCard key={card.board_id} card={card} isAuthor={true} context="my" />;
+        return (
+          <MyPageReviewCard key={card.board_id} card={card} isAuthor={true} context="my" priority={shouldPrioritize} />
+        );
       case 'myBookmarkedReviews':
         const isAuthor = card.author_email === props.userEmail;
-        return <MyPageReviewCard key={card.board_id} card={card} isAuthor={isAuthor} context="bookmarks" />;
+        return (
+          <MyPageReviewCard
+            key={card.board_id}
+            card={card}
+            isAuthor={isAuthor}
+            context="bookmarks"
+            priority={shouldPrioritize}
+          />
+        );
       case 'bestReviews':
-        return <BestReviewCard key={card.board_id} card={card} />;
+        return <BestReviewCard key={card.board_id} card={card} priority={shouldPrioritize} />;
       default:
         const _exhaustiveCheck: never = from;
         throw new Error(`허용되지 않은 'from' 값: ${_exhaustiveCheck}`);
     }
   }
 
-  return <ul className={reviewsGridVariants({variant})}>{reviews.map(card => renderCardFrame(card))}</ul>;
+  return (
+    <ul className={reviewsGridVariants({variant})}>{reviews.map((card, index) => renderCardFrame(card, index))}</ul>
+  );
 }
