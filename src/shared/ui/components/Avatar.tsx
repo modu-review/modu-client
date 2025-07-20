8ad77c7d3a873ed8e0de71a715ebed39ce272cfd
GoJiMin <gojimin3095@gmail.com>
2025-07-20 23:06:27 +0900

    [style] 사이드바의 Avatar가 각지게 표시되도록 변경

--- src/shared/ui/components/Avatar.tsx
+++ src/shared/ui/components/Avatar.tsx
@@ -3,13 +3,14 @@ import Image from 'next/image';
 type Props = {
   src: string;
   alt: string;
+  rounded?: 'rounded-full' | 'rounded-xl';
 };
 
-export default function Avatar({src, alt}: Props) {
+export default function Avatar({src, alt, rounded = 'rounded-full'}: Props) {
   return (
-    <div className="rounded-full flex justify-center items-center min-w-10 h-10 md:min-w-11 md:h-11 bg-boldBlue">
+    <div className={`flex justify-center items-center min-w-10 h-10 md:min-w-11 md:h-11 bg-boldBlue ${rounded}`}>
       <Image
-        className="rounded-full bg-white object-cover w-9 h-9 md:w-10 md:h-10"
+        className={`bg-white object-cover w-9 h-9 md:w-10 md:h-10 ${rounded}`}
         src={src}
         alt={alt}
         width={40}
--- src/widgets/side-bar/ui/UserInfo.tsx
+++ src/widgets/side-bar/ui/UserInfo.tsx
@@ -7,7 +7,7 @@ export default function UserInfo() {
 
   return (
     <div className="flex items-center gap-2">
-      <Avatar src="https://picsum.photos/200/200" alt={`${userId} 프로필 사진`} />
+      <Avatar src="https://picsum.photos/200/200" alt={`${userId} 프로필 사진`} rounded="rounded-xl" />
       <div className="flex flex-col">
         <span className="text-sm font-semibold">{userId}</span>
         <span className="text-xs text-muted-foreground">{userEmail}</span>
