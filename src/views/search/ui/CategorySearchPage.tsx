1f1c2cb450c857702a0ab616a3657a0a2e76a3dd
GoJiMin <gojimin3095@gmail.com>
2025-07-23 01:35:53 +0900

    [feat] CategoryReviews 컴포넌트의 SearchBar 컴포넌트 제거 및 CategorySearchPage 컴포넌트에서 SearchDrawer를 사용

--- src/views/search/ui/CategorySearchPage.tsx
+++ src/views/search/ui/CategorySearchPage.tsx
@@ -1,10 +1,14 @@
 import {CategoryReviews} from '@/features/reviews/category';
 import FloatingWriteButton from './FloatingWriteButton';
+import SearchDrawer from './SearchDrawer';
 
 export default function CategorySearchPage() {
   return (
     <section className="flex flex-col md:px-8 max-w-5xl mx-auto">
-      <h4 className="font-bold text-2xl mt-3 md:mt-6 mb-4 md:mb-6 md:text-3xl ml-7"> 후기글 모음 </h4>
+      <header className="flex items-center justify-between mt-3 md:mt-6 mb-4 md:mb-6 pl-7 pr-10">
+        <h4 className="font-bold text-2xl md:text-3xl"> 후기글 모음 </h4>
+        <SearchDrawer />
+      </header>
       <CategoryReviews />
       <FloatingWriteButton />
     </section>
