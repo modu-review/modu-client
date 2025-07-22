c6a65d902956c947378c1d61d259bb81ee0bfb70
GoJiMin <gojimin3095@gmail.com>
2025-07-22 20:32:35 +0900

    [style] 헤더의 양 옆 패딩 수정, 메인 페이지일 경우 border가 표시되지 않도록 수정

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import Link from 'next/link';
 import {Sidebar} from '@/widgets/side-bar';
+import {usePathname} from 'next/navigation';
 
 export default function Header() {
+  const pathName = usePathname();
+  const isMainPage = pathName === '/';
+
   return (
-    <header className="flex justify-between items-center py-5 px-6 md:px-8 lg:py-6 lg:px-10">
+    <header
+      className={`flex justify-between items-center py-5 px-1 md:px-3 lg:py-6 lg:px-8 ${isMainPage || ' border-b border-gray-200'}`}
+    >
       <Link href="/">
         <h2 className="text-2xl md:text-3xl font-bold text-boldBlue">모두의 : 후기</h2>
       </Link>
