139e1e23d719f1d11cd86248e6fd14cce582c566
GoJiMin <gojimin3095@gmail.com>
2025-07-23 01:55:32 +0900

    [style] 검색 페이지의 화면 상단 여백 수정

--- src/views/search/ui/CategorySearchPage.tsx
+++ src/views/search/ui/CategorySearchPage.tsx
@@ -5,7 +5,7 @@ import SearchDrawer from './SearchDrawer';
 export default function CategorySearchPage() {
   return (
     <section className="flex flex-col md:px-8 max-w-5xl mx-auto">
-      <header className="flex items-center justify-between mt-3 md:mt-6 mb-4 md:mb-6 pl-7 pr-10">
+      <header className="flex items-center justify-between mt-4 md:mt-6 mb-4 md:mb-6 pl-7 pr-10">
         <h4 className="font-bold text-2xl md:text-3xl"> 후기글 모음 </h4>
         <SearchDrawer />
       </header>
--- src/views/search/ui/KeywordSearchPage.tsx
+++ src/views/search/ui/KeywordSearchPage.tsx
@@ -14,7 +14,7 @@ export default async function KeywordSearchPage({params}: Props) {
 
   return (
     <section className="w-full h-full md:px-8 md:max-w-5xl mx-auto mt-2">
-      <header className="flex flex-col gap-2 mt-3 md:mt-6 mb-6 md:mb-8 ml-7 md:flex-row md:items-center md:justify-between">
+      <header className="flex flex-col gap-2 mt-4 md:mt-6 mb-6 md:mb-8 ml-7 md:flex-row md:items-center md:justify-between">
         <h2 className="font-bold text-2xl md:text-3xl">{decodedQuery} 검색 결과</h2>
         <Link href="/search" className="text-sm text-boldBlue hover:text-extraboldBlue transition-colors md:mr-4">
           카테고리별로 확인해보세요 →
