a259936fbb8a568437cee85e2acec22dfa84704c
GoJiMin <gojimin3095@gmail.com>
2025-07-25 22:27:12 +0900

    [feat] UserInfo 컴포넌트의 사용자 이미지 표시 부분을 LucideIcon이 아닌 Next/Image 컴포넌트를 사용하도록 변경
    - TODO: 백엔드에서 실제 프로필 이미지를 제공하면, src 속성 변경.
    
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/views/mypage/ui/UserInfo.tsx
+++ src/views/mypage/ui/UserInfo.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import Image from 'next/image';
 import {logout, useUserEmail, useUserId} from '@/entities/auth';
 import {Button} from '@/shared/shadcnComponent/ui/button';
-import {LucideIcon} from '@/shared/ui/icons';
 
 export default function UserInfo() {
   const userId = useUserId();
@@ -15,9 +15,17 @@ export default function UserInfo() {
   };
 
   return (
-    <section className="flex ml-2 md:ml-10">
-      <div className="translate-y-1/2 w-36 h-36 md:w-40 md:h-40 bg-white flex justify-center items-center rounded-full border-lightBlue border-[6px]">
-        <LucideIcon name="UserRound" className="w-20 h-20 md:w-32 md:h-32" />
+    <section className="flex ml-2 md:ml-10 lg:ml-16">
+      <div className="translate-y-1/2 w-36 h-36 md:w-40 md:h-40 lg:w-48 lg:h-48 bg-white flex justify-center items-center rounded-full border-lightBlue border-[6px] overflow-hidden">
+        <Image
+          // TODO: 실제 사용자 프로필 이미지로 변경
+          src="https://picsum.photos/200/200"
+          alt={`${userId} 프로필 사진`}
+          width={160}
+          height={160}
+          className="w-full h-full object-cover"
+          priority
+        />
       </div>
       <div className="mt-auto ml-3 md:ml-5 relative">
         <p className="text-lg md:text-xl text-boldBlue font-semibold mb-1 md:mb-2">{userId}</p>
