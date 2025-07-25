c3ea92bc731c380ba9175e9acf4e472889fcbc20
GoJiMin <gojimin3095@gmail.com>
2025-07-25 20:10:38 +0900

    [style] BestReviews 컴포넌트의 헤딩 문구 변경 및 화면 왼쪽에 위치하도록 변경
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/features/reviews/best/ui/BestReviews.tsx
+++ src/features/reviews/best/ui/BestReviews.tsx
@@ -16,15 +16,17 @@ export default function BestReviews({reviews}: Props) {
 
   return (
     <section className="bg-boldBlue mt-12 md:mt-16">
-      <div className="w-full max-w-[1250px] mx-auto flex flex-col items-center justify-center py-6 md:py-9 md:px-8">
-        <h4 className="text-white font-bold text-2xl mb-5 md:mb-8 md:text-3xl">🔥 BEST 후기 🔥</h4>
-        <article className="w-full max-w-[1050px] px-3 mb-2 md:mb-4">
+      <div className="w-full max-w-[1250px] mx-auto flex flex-col justify-center py-6 md:py-9 md:px-8">
+        <h4 className="text-white font-bold text-2xl text-center md:text-start md:ml-14 mb-5 md:mb-8 md:text-3xl">
+          HOT REVIEW POSTS
+        </h4>
+        <article className="w-full max-w-[1150px] mx-auto px-3 mb-2 md:mb-4">
           <CategoryBar selectedCategory={selectedCategory} onSelectCategory={handleSelectCategory} />
         </article>
         <ReviewList filteredReview={filteredReview} />
         <Link
           href="/search"
-          className="flex items-center animate-bounce cursor-pointer text-white font-bold text-[20px]"
+          className="flex items-center mx-auto animate-bounce cursor-pointer text-white font-bold text-[20px]"
           aria-label="더 많은 후기 보러가기"
         >
           더 많은 후기 보기 <LucideIcon name="ArrowRight" size={20} />
