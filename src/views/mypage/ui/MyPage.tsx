87f08ba5b41036dcbb42919617d62c66184a2291
GoJiMin <gojimin3095@gmail.com>
2025-07-25 22:29:04 +0900

    [style] MyPage 컴포넌트의 헤딩 문구 크기 조정, 사용자 정보 아래 여백 수정
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/views/mypage/ui/MyPage.tsx
+++ src/views/mypage/ui/MyPage.tsx
@@ -11,13 +11,16 @@ export default async function MyPage() {
   return (
     <section className="fixed inset-0 w-full h-full z-20 bg-white overflow-auto">
       <section className="w-full mb-4 md:mb-8">
-        <section className="bg-lightBlue p-4 flex flex-col justify-between mb-[90px] md:mb-[110px]">
+        <section className="bg-lightBlue p-4 flex flex-col justify-between mb-[90px] md:mb-[110px] lg:mb-[130px]">
           <header className="flex items-center justify-between mt-2 px-2 md:px-4">
             <div className="flex items-center gap-2">
               <Link href="/" aria-label="메인 페이지로 이동">
-                <LucideIcon name="House" className="w-7 h-7 text-boldBlue hover:scale-110 transition-transform" />
+                <LucideIcon
+                  name="House"
+                  className="w-7 h-7 lg:w-8 lg:h-8 text-boldBlue hover:scale-110 transition-transform"
+                />
               </Link>
-              <h2 className="text-2xl text-boldBlue font-bold">마이페이지</h2>
+              <h2 className="text-2xl lg:text-3xl text-boldBlue font-bold">마이페이지</h2>
             </div>
             <Sidebar />
           </header>
