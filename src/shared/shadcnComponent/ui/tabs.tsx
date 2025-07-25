8ed5239784477e662ba4bd6c13171cd4869e99cb
GoJiMin <gojimin3095@gmail.com>
2025-07-25 22:33:05 +0900

    [style] tabs 컴포넌트의 트리거 버튼 글씨 크기 수정

--- src/shared/shadcnComponent/ui/tabs.tsx
+++ src/shared/shadcnComponent/ui/tabs.tsx
@@ -8,9 +8,9 @@ import {cn} from '@/shared/lib/utils/cn';
 const Tabs = TabsPrimitive.Root;
 
 const tabsTriggerDefaultStyle =
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-base md:text-lg font-medium border-2 border-mediumBlue focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 transition-all';
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 lg:py-1.5 text-base md:text-lg lg:text-xl text-boldBlue font-medium border-2 border-mediumBlue focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 transition-all';
 const tabsTriggerActiveStyle =
-  'data-[state=active]:pb-4 md:data-[state=active]:pb-3.5 data-[state=inactive]:mb-2.5 md:data-[state=inactive]:mb-2 data-[state=active]:bg-lightBlue data-[state=active]:border-lightBlue data-[state=active]:text-boldBlue data-[state=inactive]:hover:bg-lightBlue data-[state=inactive]:hover:text-boldBlue data-[state=inactive]:hover:border-lightBlue data-[state=inactive]:hover:border-lightBlue';
+  'data-[state=active]:pb-4 md:data-[state=active]:pb-3.5 data-[state=inactive]:mb-2.5 md:data-[state=inactive]:mb-2 data-[state=active]:bg-lightBlue data-[state=active]:border-lightBlue data-[state=inactive]:hover:bg-lightBlue data-[state=inactive]:hover:text-boldBlue data-[state=inactive]:hover:border-lightBlue data-[state=inactive]:hover:border-lightBlue';
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
@@ -43,7 +43,7 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      'mt-2.5 md:mt-3 mx-1 p-3 pt-10 md:pt-16 min-h-[45vh] rounded-b-md md:min-h-[58vh] ring-offset-background bg-lightBlue focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+      'mt-2.5 md:mt-3 lg:mt-4 mx-1 p-3 pt-10 md:pt-16 min-h-[45vh] rounded-b-md md:min-h-[58vh] ring-offset-background bg-lightBlue focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
       className,
     )}
     {...props}
