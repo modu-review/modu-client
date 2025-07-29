c02b3f7f1b025579f051470f0bdc760f42d33007
GoJiMin <gojimin3095@gmail.com>
2025-07-30 01:05:50 +0900

    [add] 모달 영역에 포커스를 고정하기 위해 react-focus-lock 라이브러리 추가
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/shared/ui/modal/ui/Modal.tsx
+++ src/shared/ui/modal/ui/Modal.tsx
@@ -1,5 +1,9 @@
+'use client';
+
+import {useEffect} from 'react';
 import {createPortal} from 'react-dom';
 import {RemoveScroll} from 'react-remove-scroll';
+import FocusLock from 'react-focus-lock';
 import {LucideIcon} from '../../icons';
 
 type Props = {
@@ -8,22 +12,31 @@ type Props = {
 };
 
 export default function Modal({children, onClose}: Props) {
-  if (typeof window === 'undefined') {
-    return null;
-  }
+  useEffect(() => {
+    const handleEscapeKey = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleEscapeKey);
+    return () => document.removeEventListener('keydown', handleEscapeKey);
+  }, [onClose]);
 
   const modal = document.getElementById('modal-root') as HTMLDivElement;
 
   return (
     <RemoveScroll>
       {createPortal(
-        <section className="fixed top-0 left-0 w-full h-full z-20 flex flex-col justify-center items-center">
-          <button className="fixed z-50 top-4 right-3" onClick={onClose} aria-label="창닫기">
-            <LucideIcon name="X" className="w-6 h-6 md:w-8 md:h-8 md:text-gray-300" />
-          </button>
-          <div className="fixed top-0 left-0 w-full h-full bg-black/60" onClick={onClose} />
-          {children}
-        </section>,
+        <FocusLock>
+          <section className="fixed top-0 left-0 w-full h-full z-20 flex flex-col justify-center items-center">
+            <button className="fixed z-50 top-4 right-3" onClick={onClose} aria-label="창닫기">
+              <LucideIcon name="X" className="w-6 h-6 md:w-8 md:h-8 md:text-gray-300" />
+            </button>
+            <div className="fixed top-0 left-0 w-full h-full bg-black/60" onClick={onClose} />
+            {children}
+          </section>
+        </FocusLock>,
         modal,
       )}
     </RemoveScroll>
