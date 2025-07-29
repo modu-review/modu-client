b84a2701cee110b8decda572c1838451d428a79e
GoJiMin <gojimin3095@gmail.com>
2025-07-30 02:03:22 +0900

    [feat] 모바일 환경에서 에디터 영역 바깥에 스크롤이 생기는 문제를 해결하기 위해 스크롤락 기능 추가
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/features/review/editor/ui/Editor.tsx
+++ src/features/review/editor/ui/Editor.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import {RemoveScroll} from 'react-remove-scroll';
 import EditorMetaForm from './EditorMetaForm';
 import EditorContainer from './EditorContainer';
 import EditorFooter from './EditorFooter';
@@ -24,22 +25,24 @@ export default function Editor({title, category, content, onSave, isPending}: Pr
   });
 
   return (
-    <section className="flex flex-col w-full max-w-5xl h-full mx-auto shadow-lg pt-4">
-      <EditorMetaForm onSubmit={handleSubmit} initialTitle={title} initialCategory={category} />
-      <EditorContainer onMount={handleSetContentGetter} initialContent={content} />
-      <EditorFooter onPreview={handleSetActionPreview} onSave={handleSetActionSave} isPending={isPending} />
-      {openModal && preview && (
-        <Modal onClose={handleModalClose}>
-          <ViewerModal>
-            <Viewer {...preview} />
-          </ViewerModal>
-        </Modal>
-      )}
-      {isPending && (
-        <div className="fixed inset-0 w-full h-full z-50 bg-black/70 text-white">
-          <LoadingSpinner text="리뷰를 저장하고 있어요." />
-        </div>
-      )}
-    </section>
+    <RemoveScroll className="h-full">
+      <section className="flex flex-col w-full max-w-5xl h-full mx-auto shadow-lg pt-4">
+        <EditorMetaForm onSubmit={handleSubmit} initialTitle={title} initialCategory={category} />
+        <EditorContainer onMount={handleSetContentGetter} initialContent={content} />
+        <EditorFooter onPreview={handleSetActionPreview} onSave={handleSetActionSave} isPending={isPending} />
+        {openModal && preview && (
+          <Modal onClose={handleModalClose}>
+            <ViewerModal>
+              <Viewer {...preview} />
+            </ViewerModal>
+          </Modal>
+        )}
+        {isPending && (
+          <div className="fixed inset-0 w-full h-full z-50 bg-black/70 text-white">
+            <LoadingSpinner text="리뷰를 저장하고 있어요." />
+          </div>
+        )}
+      </section>
+    </RemoveScroll>
   );
 }
