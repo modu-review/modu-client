90de40eb5b23cbde40b9b8a00edcb60a9c23467d
GoJiMin <gojimin3095@gmail.com>
2025-07-30 01:21:42 +0900

    [style] 헤더 패딩 수정, 뱃지 컴포넌트의 위아래 크기 수정
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/app/layouts/index.tsx
+++ src/app/layouts/index.tsx
@@ -26,7 +26,7 @@ export function RootLayout({
     <html lang="en">
       <body className={`${pretandard.className} antialiased flex flex-col w-full`}>
         <Providers>
-          <header className="sticky top-0 z-10 px-5 bg-white/85 backdrop-blur-xl">
+          <header className="sticky top-0 z-10 bg-white/85 backdrop-blur-xl">
             <Header />
           </header>
           <main className="grow w-full mx-auto">{children}</main>
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
 
   return (
     <header
-      className={`flex justify-between items-center py-5 px-1 md:px-3 lg:py-6 lg:px-8 ${isMainPage || ' border-b border-gray-200'}`}
+      className={`flex justify-between items-center py-5 px-5 md:px-8 lg:py-6 lg:px-10 ${isMainPage || ' border-b border-gray-200'}`}
     >
       <Link href="/">
         <h2 className="text-2xl md:text-3xl font-bold text-boldBlue">모두의 : 후기</h2>
