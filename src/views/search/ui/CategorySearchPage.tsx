6be519561098827edc2f5c1a88be52a1011ec2fd
GoJiMin <gojimin3095@gmail.com>
2025-07-29 23:04:21 +0900

    [feat] 데스크탑 환경에선 검색바를 바로 표시하며, 모바일 환경에선 검색 드로어를 표시하도록 변경
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/views/search/ui/CategorySearchPage.tsx
+++ src/views/search/ui/CategorySearchPage.tsx
@@ -7,7 +7,9 @@ export default function CategorySearchPage() {
     <section className="flex flex-col md:px-8 max-w-5xl mx-auto">
       <header className="flex items-center justify-between mt-4 md:mt-6 mb-4 md:mb-6 pl-7 pr-10">
         <h4 className="font-bold text-2xl md:text-3xl"> 후기글 모음 </h4>
-        <SearchDrawer />
+        <div className="md:hidden">
+          <SearchDrawer />
+        </div>
       </header>
       <CategoryReviews />
       <FloatingWriteButton />
