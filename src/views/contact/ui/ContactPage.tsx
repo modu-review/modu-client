a098b394a22d03b88685d5633dd404de5d082c03
queenJiJi <lovebellaberry52@gmail.com>
2025-08-01 02:34:39 +0900

    [feat] 문의폼 유효성 검증 로직 추가 -useForm  활용

--- src/views/contact/ui/ContactPage.tsx
+++ src/views/contact/ui/ContactPage.tsx
@@ -1,37 +1,61 @@
+'use client';
+
 import React from 'react';
+import {useForm} from 'react-hook-form';
+
+type Form = {
+  name: string;
+  email: string;
+  message: string;
+};
 
 export default function ContactPage() {
+  const {
+    handleSubmit,
+    register,
+    formState: {errors},
+  } = useForm<Form>({});
+
+  const onValid = async (data: Form) => {
+    console.log('Form Data:', data);
+  };
+
   return (
     <div className="w-full max-w-md mt-6 p-6 bg-white rounded-lg shadow-md">
-      <form className="flex flex-col space-y-4 w-full">
+      <form onSubmit={handleSubmit(onValid)} className="flex flex-col space-y-4 w-full">
         <h2 className="text-2xl font-bold mb-6 text-gray-800">Contact Me </h2>
         <label htmlFor="name">이름: </label>
         <input
+          {...register('name', {required: '이름을 입력해주세요. '})}
           type="text"
-          name="name"
           id="name"
           autoFocus
           className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300"
         />
-
+        {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
         <label htmlFor="email">이메일: </label>
         <input
           type="email"
           id="email"
-          name="email"
-          required
+          {...register('email', {
+            required: '이메일을 입력해주세요.',
+            pattern: {
+              value: /^\S+@\S+$/i,
+              message: '올바른 이메일 형식이 아닙니다.',
+            },
+          })}
           className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300"
         />
+        {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
 
         <label htmlFor="message">문의 내용: </label>
         <textarea
           rows={10}
           id="message"
-          name="message"
-          required
+          {...register('message', {required: '문의 내용을 입력해주세요.'})}
           className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300"
         />
-
+        {errors.message && <p className="text-red-500 text-sm">{errors.message.message}</p>}
         <button type="submit" className="bg-pink-500 text-white font-bold py-3 rounded-lg hover:bg-pink-600 transition">
           전송
         </button>
