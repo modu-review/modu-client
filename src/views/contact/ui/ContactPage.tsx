475a6c7d16da8f2589f4bb87b7e989fb9128365b
queenJiJi <lovebellaberry52@gmail.com>
2025-08-01 17:19:48 +0900

    [feat] 폼의 UI를 shadcnUI로 변경

--- src/views/contact/ui/ContactPage.tsx
+++ src/views/contact/ui/ContactPage.tsx
@@ -4,6 +4,10 @@ import {Modal, useModal} from '@/shared/ui/modal';
 import React from 'react';
 import {useForm} from 'react-hook-form';
 import AlertModal from './AlertModal';
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from '@/shared/shadcnComponent/ui/form';
+import {Input} from '@/shared/shadcnComponent/ui/input';
+import {Textarea} from '@/shared/shadcnComponent/ui/textarea';
+import {Button} from '@/shared/shadcnComponent/ui/button';
 
 type Form = {
   name: string;
@@ -14,13 +18,14 @@ type Form = {
 export default function ContactPage() {
   const {openModal, handleModalOpen, handleModalClose} = useModal();
 
-  const {
-    handleSubmit,
-    register,
-    reset,
-    getValues,
-    formState: {errors},
-  } = useForm<Form>({});
+  const form = useForm<Form>({
+    defaultValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+  });
+  const {reset, getValues} = form;
 
   // 취소버튼 눌렀을 시
   const handleCancel = () => {
@@ -42,44 +47,77 @@ export default function ContactPage() {
 
   return (
     <div className="w-full max-w-md mt-6 p-6 bg-white rounded-lg shadow-md">
-      <form onSubmit={handleSubmit(onValid)} className="flex flex-col space-y-4 w-full">
-        <h2 className="text-2xl font-bold mb-6 text-gray-800">Contact Me </h2>
-        <label htmlFor="name">이름: </label>
-        <input
-          {...register('name', {required: '이름을 입력해주세요. '})}
-          type="text"
-          id="name"
-          autoFocus
-          className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300"
-        />
-        {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
-        <label htmlFor="email">이메일: </label>
-        <input
-          type="email"
-          id="email"
-          {...register('email', {
-            required: '이메일을 입력해주세요.',
-            pattern: {
-              value: /^\S+@\S+$/i,
-              message: '올바른 이메일 형식이 아닙니다.',
-            },
-          })}
-          className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300"
-        />
-        {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
-
-        <label htmlFor="message">문의 내용: </label>
-        <textarea
-          rows={10}
-          id="message"
-          {...register('message', {required: '문의 내용을 입력해주세요.'})}
-          className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300"
-        />
-        {errors.message && <p className="text-red-500 text-sm">{errors.message.message}</p>}
-        <button type="submit" className="bg-pink-500 text-white font-bold py-3 rounded-lg hover:bg-pink-600 transition">
-          전송
-        </button>
-      </form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onValid)} className="flex flex-col space-y-4 w-full">
+          <h2 className="text-2xl font-bold mb-6 text-gray-800">Contact Me </h2>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>이름: </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="이름을 입력해주세요."
+                    className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300"
+                    {...form.register('name', {required: '이름을 입력해주세요. '})}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>이메일: </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="이메일을 입력해주세요."
+                    className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300"
+                    {...form.register('email', {
+                      required: '이메일을 입력해주세요.',
+                      pattern: {
+                        value: /^\S+@\S+$/i,
+                        message: '올바른 이메일 형식이 아닙니다.',
+                      },
+                    })}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="message"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>문의 내용: </FormLabel>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder="문의 내용을 입력해주세요."
+                    className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300"
+                    {...form.register('message', {required: '문의 내용을 입력해주세요.'})}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="bg-pink-500 text-white font-bold py-3 rounded-lg hover:bg-pink-600 transition"
+          >
+            전송
+          </Button>
+        </form>
+      </Form>
       {openModal && (
         <Modal onClose={handleModalClose}>
           <AlertModal onCancel={handleCancel} onConfirm={handleConfirm} message={'전송을 완료하시겠습니까?'} />
