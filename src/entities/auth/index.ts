f4a5ce54a04448b85e9b3dede95e6b88287ecc8a
GoJiMin <gojimin3095@gmail.com>
2025-08-03 02:36:15 +0900

    [feat] 로그아웃 요청을 리액트 쿼리와 통합하기 위해 useLogout mutation 훅 구현

--- src/entities/auth/index.ts
+++ src/entities/auth/index.ts
@@ -5,3 +5,4 @@ export {getSession} from './apis/api-service';
 export {login, logout} from './apis/api-service';
 export {useGetSession} from './model/useGetSession';
 export {useLogin} from './model/useLogin';
+export {useLogout} from './model/useLogout';
