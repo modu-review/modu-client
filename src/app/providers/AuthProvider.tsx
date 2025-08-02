76a8ea955fa4242b631182aa8f11e3870e9dc5b9
GoJiMin <gojimin3095@gmail.com>
2025-08-03 01:57:32 +0900

    [feat] AuthProvider에서 getSession을 직접 호출하지 않고 useGetSession 훅을 통해 데이터를 반환하도록 변경, AuthProvider가 ReactQueryProvider 내부에 위치하도록 변경

--- src/app/providers/AuthProvider.tsx
+++ src/app/providers/AuthProvider.tsx
@@ -1,18 +1,31 @@
 'use client';
 
 import {useEffect} from 'react';
-import {getSession, useUpdateUser} from '@/entities/auth';
+import {useGetSession, useUpdateUser} from '@/entities/auth';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function AuthProvider({children}: Props) {
+  const {session, isSuccess} = useGetSession();
   const updateUser = useUpdateUser();
 
   useEffect(() => {
-    getSession().then(updateUser);
-  }, [updateUser]);
+    if (isSuccess && session) {
+      updateUser({
+        userId: session.userId,
+        userEmail: session.userEmail,
+        isLoggedIn: true,
+      });
+    } else {
+      updateUser({
+        userId: null,
+        userEmail: null,
+        isLoggedIn: false,
+      });
+    }
+  }, [updateUser, session, isSuccess]);
 
   return children;
 }
