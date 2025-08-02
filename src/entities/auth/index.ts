754fc3b0d7521c3e5670f9607fe53b9df34b5849
GoJiMin <gojimin3095@gmail.com>
2025-08-03 01:50:54 +0900

    [feat] 사용자 정보 요청을 위한 쿼리 훅 구현

--- src/entities/auth/index.ts
+++ src/entities/auth/index.ts
@@ -3,3 +3,4 @@ export * from './consts/authConstants';
 export {useIsLoggedIn, useUserEmail, useUserId, useUpdateUser} from './model/authStore';
 export {getSession} from './apis/api-service';
 export {login, logout} from './apis/api-service';
+export {useGetSession} from './model/useGetSession';
