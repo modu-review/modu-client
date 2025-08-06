393923e287fcc4015fe6cafe33a17d43e5b68fb3
GoJiMin <gojimin3095@gmail.com>
2025-08-07 01:37:25 +0900

    [feat] 후기 작성 관련 반환 에러 코드 추가

--- src/shared/lib/consts/errorMessage.ts
+++ src/shared/lib/consts/errorMessage.ts
@@ -34,6 +34,15 @@ export const SERVER_ERROR_MESSAGE: ErrorMessage = {
    */
   BOARD_ID_NOT_FOUND: '해당 후기를 찾을 수 없어요. 후기가 삭제되었거나 존재하지 않을 수 있어요.',
   BOARD_NOT_EXIST: '해당 후기를 찾을 수 없어요. 후기가 삭제되었거나 존재하지 않을 수 있어요.',
+
+  /**
+   * 후기 작성 관련
+   * 이미지 임시 업로드 URL 발급 POST /presign
+   * 후기 저장 POST /reviews/new
+   */
+  CAN_NOT_CREATE_PRESIGNED_URL: '이미지 업로드를 위한 임시 URL을 생성할 수 없어요. 다시 시도해주세요.',
+  CAN_NOT_CREATE_UUID: '이미지 아이디를 생성하는 데 실패했어요. 다시 시도해주세요.',
+  NOT_ALLOWED_HTML_ERROR: '잘못된 HTML 형식이에요. 올바른 HTML을 입력해주세요.',
 } as const;
 
 export const ERROR_MESSAGE = {
