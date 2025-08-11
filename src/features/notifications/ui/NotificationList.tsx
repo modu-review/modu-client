f4956b64c027dd6429db99c1ccc593305cef4081
GoJiMin <gojimin3095@gmail.com>
2025-08-11 23:06:31 +0900

    [feat] NotificationList 컴포넌트 기본 구조 구현 및 알림 컴포넌트 연결

---
+++ src/features/notifications/ui/NotificationList.tsx
@@ -0,0 +1,7 @@
+export default function NotificationList() {
+  return (
+    <ul className="min-h-[80%] mt-6 md:mt-8 mx-4 md:mx-0 bg-white rounded-lg shadow-md shadow-gray-300">
+      {/* TODO: 알림 조회 API 연동 후 데이터 매핑 */}
+    </ul>
+  );
+}
--- src/features/notifications/ui/Notifications.tsx
+++ src/features/notifications/ui/Notifications.tsx
@@ -1,9 +1,11 @@
 import NotificationHeader from './NotificationHeader';
+import NotificationList from './NotificationList';
 
 export default function Notifications() {
   return (
     <section className=" h-full bg-gray-100 rounded-lg pt-7 md:px-8">
       <NotificationHeader />
+      <NotificationList />
     </section>
   );
 }
