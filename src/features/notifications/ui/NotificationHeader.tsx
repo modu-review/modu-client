2f716bf7b4a0c878d59d3691c42851c1c2387a63
GoJiMin <gojimin3095@gmail.com>
2025-08-11 22:57:31 +0900

    [style] NotificationHeader 스타일링

--- src/features/notifications/ui/NotificationHeader.tsx
+++ src/features/notifications/ui/NotificationHeader.tsx
@@ -1,9 +1,9 @@
 export default function NotificationHeader() {
   return (
-    <header>
-      <p>알림 내용</p>
-      <p>시간</p>
-      <p>상태</p>
+    <header className="flex items-center bg-white rounded-full mx-3 md:mx-0 px-6 md:px-8 py-4 text-sm md:text-base font-semibold shadow-md shadow-gray-400">
+      <p className="flex-[0.5] ml-3 md:ml-8">알림 내용</p>
+      <p className="flex-[0.3] text-center">시간</p>
+      <p className="flex-[0.2] text-center">상태</p>
     </header>
   );
 }
