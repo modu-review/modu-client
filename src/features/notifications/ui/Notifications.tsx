d4cc1946361e1e6ddb68d42d5617d5cb10aaac61
GoJiMin <gojimin3095@gmail.com>
2025-08-11 23:00:45 +0900

    [style] 알림 페이지 및 알림 컴포넌트의 높이를 항상 가득 채우도록 설정, 알림 컴포넌트의 배경 색을 회색으로 설정

--- src/features/notifications/ui/Notifications.tsx
+++ src/features/notifications/ui/Notifications.tsx
@@ -2,7 +2,7 @@ import NotificationHeader from './NotificationHeader';
 
 export default function Notifications() {
   return (
-    <section>
+    <section className=" h-full bg-gray-100 rounded-lg pt-7 md:px-8">
       <NotificationHeader />
     </section>
   );
--- src/views/notifications/ui/NotificationsPage.tsx
+++ src/views/notifications/ui/NotificationsPage.tsx
@@ -2,8 +2,8 @@ import {Notifications} from '@/features/notifications';
 
 export default function NotificationsPage() {
   return (
-    <section className="flex flex-col max-w-5xl mx-auto px-4 pt-7">
-      <h2 className="text-2xl font-semibold ml-6">알림</h2>
+    <section className="h-full flex flex-col max-w-5xl mx-auto md:px-4 pt-7">
+      <h2 className="text-2xl font-semibold ml-6 mb-4 md:mb-6">알림</h2>
       <Notifications />
     </section>
   );
