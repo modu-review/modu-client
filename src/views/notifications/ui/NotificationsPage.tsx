745579d78c1bf6444d05fa5c6ee88c30066f2e1b
GoJiMin <gojimin3095@gmail.com>
2025-08-11 22:47:02 +0900

    [feat] 알림 컴포넌트 추가 및 알림 페이지에 연결

--- src/views/notifications/ui/NotificationsPage.tsx
+++ src/views/notifications/ui/NotificationsPage.tsx
@@ -1,7 +1,10 @@
+import {Notifications} from '@/features/notifications';
+
 export default function NotificationsPage() {
   return (
     <section className="flex flex-col max-w-5xl mx-auto px-4 pt-7">
       <h2 className="text-2xl font-semibold ml-6">알림</h2>
+      <Notifications />
     </section>
   );
 }
