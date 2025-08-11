fda96bf6ce588356867021d69d7394fee8ade71d
GoJiMin <gojimin3095@gmail.com>
2025-08-11 23:38:06 +0900

    [fix] getNotifications 함수의 API 엔드포인트를 /api/users/me/notifications로 변경
    - Mock API 조회를 위해 /api 추가

--- src/entities/notifications/apis/api-service.ts
+++ src/entities/notifications/apis/api-service.ts
@@ -5,6 +5,6 @@ export async function getNotifications() {
   return await requestGet<Notification[]>({
     // TODO: 개발 완료 후 baseUrl 제거.
     baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
-    endpoint: '/users/me/notifications',
+    endpoint: '/api/users/me/notifications',
   });
 }
