44f26601bba6d60eeb0681af6f847867bb3ea183
GoJiMin <gojimin3095@gmail.com>
2025-08-11 22:40:41 +0900

    [feat] 알림 페이지 이동을 위한 헤더 내 알림 아이콘 추가

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import {Sidebar} from '@/widgets/side-bar';
 import {usePathname} from 'next/navigation';
+import {LucideIcon} from '@/shared/ui/icons';
 
 export default function Header() {
   const pathName = usePathname();
@@ -15,7 +16,15 @@ export default function Header() {
       <Link href="/">
         <h2 className="text-2xl md:text-3xl font-bold text-boldBlue">모두의 : 후기</h2>
       </Link>
-      <Sidebar />
+      <div className="flex items-center gap-4 md:gap-6">
+        <Link href="/notifications">
+          <LucideIcon
+            name="Bell"
+            className="w-5 h-5 md:w-6 md:h-6 hover:text-boldBlue md:hover:scale-105 transition-all"
+          />
+        </Link>
+        <Sidebar />
+      </div>
     </header>
   );
 }
