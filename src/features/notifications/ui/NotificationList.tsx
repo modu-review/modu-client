6386feab8f42c757f9457f8e3e34002da4f39f0f
GoJiMin <gojimin3095@gmail.com>
2025-08-12 23:50:47 +0900

    [feat] 알림이 존재하지 않을 경우 보여질 UI 구성
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/features/notifications/ui/NotificationList.tsx
+++ src/features/notifications/ui/NotificationList.tsx
@@ -6,6 +6,15 @@ import {useGetNotifications} from '@/entities/notifications';
 export default function NotificationList() {
   const {data} = useGetNotifications();
 
+  if (data.length === 0) {
+    return (
+      <section className="min-h-[80%] max-h-[310px] md:max-h-[600px] flex flex-col items-center justify-center mt-6 md:mt-8 mx-2 md:mx-10 pb-24 md:pb-14 bg-white rounded-lg shadow-md shadow-gray-300">
+        <h2 className="text-xl md:text-2xl font-semibold mb-1">아직 알림이 없어요.</h2>
+        <p className="text-sm md:text-base text-muted-foreground">새로운 알림이 도착하면 여기에서 확인할 수 있어요.</p>
+      </section>
+    );
+  }
+
   return (
     <ul className="min-h-[80%] max-h-[310px] md:max-h-[600px] mt-6 md:mt-8 mx-2 md:mx-10 bg-white rounded-lg shadow-md shadow-gray-300 overflow-y-auto">
       {data.map(notification => (
