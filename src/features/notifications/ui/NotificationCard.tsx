8760d74c12636c31a11e9a5f4a7653337e4b3b8e
GoJiMin <gojimin3095@gmail.com>
2025-08-12 21:53:14 +0900

    [feat] NotificationCard 컴포넌트 내 알림 처리 훅 연결 및 접근성을 위해 article 태그를 button 요소로 변경, aria 속성 적용

--- src/features/notifications/ui/NotificationCard.tsx
+++ src/features/notifications/ui/NotificationCard.tsx
@@ -1,6 +1,8 @@
-import Link from 'next/link';
-import {Notification} from '@/entities/notifications';
+'use client';
+
+import {Notification, useMarkNotificationAsRead} from '@/entities/notifications';
 import {LucideIcon} from '@/shared/ui/icons';
+import {useRouter} from 'next/navigation';
 
 type Props = {
   notification: Notification;
@@ -25,24 +27,36 @@ export default function NotificationCard({notification}: Props) {
   const {id, board_id, isRead, title, created_at, type} = notification;
   const config = NOTIFICATION_CONFIG[type];
 
+  const router = useRouter();
+
+  const {markNotificationAsRead} = useMarkNotificationAsRead();
+
+  const handleMarkAsRead = () => {
+    if (isRead) {
+      router.push(`/reviews/${board_id}`);
+    } else {
+      markNotificationAsRead({notificationId: id, boardId: board_id});
+    }
+  };
+
   return (
     <>
-      <Link href={`/reviews/${board_id}`}>
-        <article
-          className={`w-full flex items-center border-b-2 border-neutral-300 ${notification.isRead ? 'bg-gray-200' : 'bg-white'} py-5 px-3 md:px-5 pb-8 md:pb-6 hover:bg-gray-100 transition-colors`}
-        >
-          <div className={`${config.bgColor} p-2 md:p-2 rounded-lg mr-3 md:mr-3`}>
-            <LucideIcon name={config.icon} className="w-4 h-4 md:w-5 md:h-5 text-white" />
-          </div>
-          <div className="flex flex-col flex-1 md:flex-[0.75]">
-            <p className="font-semibold">{config.title}</p>
-            <p className="text-[13px] md:text-sm text-muted-foreground">{config.getMessage(title)}</p>
-          </div>
-          <p className="absolute right-3 bottom-2 md:static text-[12px] md:text-sm font-semibold flex-[0.2] whitespace-nowrap">
-            {created_at}
-          </p>
-        </article>
-      </Link>
+      <button
+        className={`w-full text-left flex items-center border-b-2 border-neutral-300 ${notification.isRead ? 'bg-gray-200' : 'bg-white'} py-5 px-3 md:px-5 pb-8 md:pb-6 hover:bg-gray-100 transition-colors cursor-pointer`}
+        onClick={handleMarkAsRead}
+        aria-label={`${config.title} - ${config.getMessage(title)} 게시글로 이동`}
+      >
+        <div className={`${config.bgColor} p-2 md:p-2 rounded-lg mr-3 md:mr-3`}>
+          <LucideIcon name={config.icon} className="w-4 h-4 md:w-5 md:h-5 text-white" />
+        </div>
+        <div className="flex flex-col flex-1 md:flex-[0.75]">
+          <p className="font-semibold">{config.title}</p>
+          <p className="text-[13px] md:text-sm text-muted-foreground">{config.getMessage(title)}</p>
+        </div>
+        <p className="absolute right-3 bottom-2 md:static text-[12px] md:text-sm font-semibold flex-[0.2] whitespace-nowrap">
+          {created_at}
+        </p>
+      </button>
       {/* TODO: 알림 삭제 기능 연결 */}
       <button className="absolute top-2 md:inset-y-0 md:translate-y-[-4%] right-1 md:right-3">
         <LucideIcon name="X" className="w-5 h-5 md:w-5 md:h-5 text-gray-500 hover:text-gray-700" />
