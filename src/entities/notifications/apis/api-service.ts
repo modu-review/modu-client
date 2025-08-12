1513b296749aab49cdea83489340fac4a5eceb91
GoJiMin <gojimin3095@gmail.com>
2025-08-12 21:14:59 +0900

    [feat] 알림 읽음 처리를 위한 API 구현

--- src/entities/notifications/apis/api-service.ts
+++ src/entities/notifications/apis/api-service.ts
@@ -1,10 +1,21 @@
 import {Notification} from '../model/type';
-import {requestGet} from '@/shared/apis';
+import {requestGet, requestPatch} from '@/shared/apis';
 
 export async function getNotifications() {
   return await requestGet<Notification[]>({
     // TODO: 개발 완료 후 baseUrl 제거.
     baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
     endpoint: '/api/users/me/notifications',
+  });
+}
+
+export async function markNotificationAsRead(notificationId: string) {
+  await requestPatch({
+    // TODO: 개발 완료 후 baseUrl 제거.
+    baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
+    endpoint: `/api/users/me/notifications/${notificationId}`,
+    body: {
+      isRead: true,
+    },
   });
 }
