dc4a4fd59a36263e7a308f6abdcf22e967f675ed
queenJiJi <lovebellaberry52@gmail.com>
2025-08-14 01:24:08 +0900

    [feat] PostsByUserPage의 구성요소 UI 경로설정 및 레이아웃 구성

--- src/views/users/reviews/ui/PostsByUserPage.tsx
+++ src/views/users/reviews/ui/PostsByUserPage.tsx
@@ -1,3 +1,20 @@
-export default function PostsByUserPage() {
-  return;
+import {Suspense} from 'react';
+import UserInfo from './UserInfo';
+import {UserPostsList} from '@/features/users/posts';
+
+type Props = {
+  params: Promise<{userId: string}>;
+};
+export default async function PostsByUserPage({params}: Props) {
+  const {userId} = await params;
+
+  return (
+    <section>
+      {/* <UserInfo /> TODO: 최상단 프로필 사진구현 */}
+
+      {/* <Suspense fallback={<UserPostsLoading />}> //TODO: 로딩시 skeleton UI 구현
+            <UserPostsList /> //TODO: 프로필 하단에 사용자별 게시글보여주기 UI 구현
+        </Suspense> */}
+    </section>
+  );
 }
