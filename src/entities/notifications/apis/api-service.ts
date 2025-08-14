beec5e4938324c23b598f4f4bbdc22fcb6b4044c
GoJiMin <gojimin3095@gmail.com>
2025-08-14 13:18:04 +0900

    [feat] 알림 요청 API의 인자로 page를 받도록 수정, 요청 시 queryParam으로 page를 전달하도록 변경
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/entities/notifications/apis/api-service.ts
+++ src/entities/notifications/apis/api-service.ts
@@ -1,11 +1,14 @@
 import {Notification} from '../model/type';
 import {requestGet, requestPatch} from '@/shared/apis';
 
-export async function getNotifications() {
+export async function getNotifications(page: number) {
   return await requestGet<Notification[]>({
     // TODO: 개발 완료 후 baseUrl 제거.
     baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
     endpoint: '/api/users/me/notifications',
+    queryParams: {
+      page: page,
+    },
   });
 }
 
