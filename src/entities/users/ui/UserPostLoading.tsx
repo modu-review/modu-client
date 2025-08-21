d29e27f4a843912fda69da22f71df33881756598
GoJiMin <gojimin3095@gmail.com>
2025-08-21 15:46:02 +0900

    [style] 변경된 UserPost와 동일하게 스켈레톤 UI 수정
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/entities/users/ui/UserPostLoading.tsx
+++ src/entities/users/ui/UserPostLoading.tsx
@@ -2,35 +2,30 @@ import {Skeleton} from '@/shared/shadcnComponent/ui/skeleton';
 
 export default function UserPostLoading() {
   return (
-    <li>
-      <div className="flex p-4 md:p-6 border border-gray-200 rounded-lg shadow-sm mb-4">
-        {/* 썸네일 이미지 Skeleton */}
-        <div className="flex-shrink-0 w-[100px] h-[100px] overflow-hidden rounded-md border mr-4">
-          <Skeleton className="w-full h-full" />
+    <div className="flex w-full bg-slate-100 rounded-xl shadow px-3 py-5 md:p-6">
+      {/* 썸네일 이미지 Skeleton */}
+      <Skeleton className="w-[100px] h-[100px] rounded-md mr-3" />
+
+      {/* 텍스트 콘텐츠 영역 */}
+      <div className="flex flex-col flex-1">
+        {/* 제목 + 카테고리 */}
+        <div className="flex justify-between items-center">
+          <Skeleton className="w-[150px] md:w-[250px] h-5 mb-2" />
+          <Skeleton className="hidden md:block w-[85px] h-7 rounded-xl" />
         </div>
 
-        {/* 텍스트 콘텐츠 영역 */}
-        <div className="flex flex-col flex-1">
-          {/* 제목 + 카테고리 */}
-          <div className="flex justify-between items-start">
-            <Skeleton className="w-2/3 h-5 mb-2" />
-            <Skeleton className="w-[85px] h-6 rounded-xl" />
-          </div>
+        {/* 날짜 */}
+        <Skeleton className="w-[130px] h-3 mb-3" />
 
-          {/* 날짜 */}
-          <Skeleton className="w-[100px] h-3 mb-3" />
+        {/* 본문 요약 */}
+        <Skeleton className="w-full h-9 mb-1" />
 
-          {/* 본문 요약 */}
-          <Skeleton className="w-full h-4 mb-1" />
-          <Skeleton className="w-3/4 h-4 mb-3" />
-
-          {/* 하단 아이콘 영역 */}
-          <div className="flex gap-6 mt-2">
-            <Skeleton className="w-[40px] h-[20px]" />
-            <Skeleton className="w-[40px] h-[20px]" />
-          </div>
+        {/* 하단 아이콘 영역 */}
+        <div className="flex gap-4 mt-2">
+          <Skeleton className="w-[30px] h-[20px]" />
+          <Skeleton className="w-[30px] h-[20px]" />
         </div>
       </div>
-    </li>
+    </div>
   );
 }
