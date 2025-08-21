424a8a0f626ac888d80d972161214c68b35294b9
GoJiMin <gojimin3095@gmail.com>
2025-08-22 01:57:40 +0900

    [feat] 마이페이지의 UserInfo 컴포넌트에서 ProfileImage 컴포넌트를 사용하도록 변경

--- src/views/mypage/ui/UserInfo.tsx
+++ src/views/mypage/ui/UserInfo.tsx
@@ -1,25 +1,21 @@
 'use client';
 
-import Image from 'next/image';
 import {useUserNickname} from '@/entities/auth';
 import {Skeleton} from '@/shared/shadcnComponent/ui/skeleton';
+import {Suspense} from 'react';
+import ProfileImage from '@/features/users/profileImage/ui/ProfileImage';
 
 export default function UserInfo() {
   const userNickname = useUserNickname();
 
   return (
     <section className="flex flex-col items-center mt-10 md:mt-8 lg:mt-10">
-      <div className="w-36 h-36 md:w-40 md:h-40 lg:w-48 lg:h-48 bg-white flex justify-center items-center rounded-full border-boldBlue border-[7px] overflow-hidden">
-        <Image
-          // TODO: 실제 사용자 프로필 이미지로 변경
-          src="https://picsum.photos/seed/ee2/200/200"
-          alt={`${userNickname} 프로필 사진`}
-          width={160}
-          height={160}
-          className="w-full h-full object-cover"
-          priority
+      <Suspense fallback={<div>loading...</div>}>
+        <ProfileImage
+          userNickname={userNickname}
+          className="w-36 h-36 md:w-40 md:h-40 lg:w-48 lg:h-48 flex justify-center items-center rounded-full border-boldBlue border-[7px] overflow-hidden"
         />
-      </div>
+      </Suspense>
       <div className="flex flex-col items-center mt-3">
         {userNickname ? (
           <p className="text-xl lg:text-2xl font-bold">{userNickname}</p>
