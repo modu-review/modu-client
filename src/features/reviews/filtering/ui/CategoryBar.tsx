7b48462e7de9a43ec5ebc9ce293e1f2fda44afde
GoJiMin <gojimin3095@gmail.com>
2025-08-22 17:35:29 +0900

    [style] 카테고리 바가 모바일 환경에서 한줄로 표시되며 스크롤을 통해 넘길 수 있게 변경
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/features/reviews/filtering/ui/CategoryBar.tsx
+++ src/features/reviews/filtering/ui/CategoryBar.tsx
@@ -1,5 +1,6 @@
 import {Category, CATEGORY_LIST} from '@/entities/review';
 import {Button} from '@/shared/shadcnComponent/ui/button';
+import {LucideIcon} from '@/shared/ui/icons';
 
 type Props = {
   selectedCategory: Category;
@@ -8,11 +9,12 @@ type Props = {
 
 export default function CategoryBar({selectedCategory, onSelectCategory}: Props) {
   return (
-    <nav className="w-full px-2 mb-8 md:mb-10">
-      <ul className="grid grid-cols-[repeat(auto-fit,minmax(100px,1fr))] text-center gap-2 bg-white rounded-3xl p-3 border shadow-md">
+    <nav className="w-full px-2 mb-8 md:mb-10 relative">
+      <ul className="flex overflow-x-auto xs:grid xs:grid-cols-[repeat(auto-fit,minmax(100px,1fr))] text-center gap-2 bg-white rounded-3xl p-3 border shadow-md [scrollbar-width:none] [&::-webkit-scrollbar]:hidden;">
         {CATEGORY_LIST.map(({id, value, label}) => (
           <li key={id}>
             <Button
+              className="w-[90px] md:w-full"
               variant={selectedCategory === value ? 'activeCategory' : 'inActiveCategory'}
               size="category"
               aria-pressed={selectedCategory === value}
@@ -24,6 +26,10 @@ export default function CategoryBar({selectedCategory, onSelectCategory}: Props)
           </li>
         ))}
       </ul>
+      <LucideIcon
+        name="ChevronRight"
+        className="block xs:hidden absolute right-2 top-[19px] text-muted-foreground font-bold text-xl"
+      />
     </nav>
   );
 }
