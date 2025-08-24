2148d7ce0b588e570d862d1acea123d2003254d4
GoJiMin <gojimin3095@gmail.com>
2025-08-24 21:59:08 +0900

    [feat] 이미지 파일 크기 및 형식 검증 기능 구현, 파일이 존재할 경우 프리뷰 이미지를 보이기 위해 조건부 렌더링 적용

--- src/features/users/profileImage/ui/ChangeProfileImageDialog.tsx
+++ src/features/users/profileImage/ui/ChangeProfileImageDialog.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import {useUpdateGlobalError} from '@/entities/error';
+import {createClientError} from '@/shared/lib/utils/client-error';
 import {
   Dialog,
   DialogContent,
@@ -9,12 +12,49 @@ import {
 } from '@/shared/shadcnComponent/ui/dialog';
 import {ImageUploadDragArea} from '@/shared/ui/components';
 import {LucideIcon} from '@/shared/ui/icons';
+import {ChangeEvent, useState} from 'react';
+
+const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png'];
+const MAX_SIZE_EXCEEDED = 5 * 1024 * 1024;
 
 export default function ChangeProfileImageDialog() {
+  const [file, setFile] = useState<File | null>(null);
+
   const updateGlobalError = useUpdateGlobalError();
 
-  const handleFileUpload = (file: File) => {
-    // TODO: 파일 업로드 로직 구현
+  const validateFile = (file: File) => {
+    if (!ACCEPTED_IMAGE_TYPES.includes(file.type)) {
+      updateGlobalError(createClientError('NOT_SUPPORTED_FILE'));
+
+      return false;
+    }
+
+    if (file.size > MAX_SIZE_EXCEEDED) {
+      updateGlobalError(createClientError('FILE_SIZE_EXCEEDED'));
+
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleDropImage = (file: File) => {
+    if (validateFile(file)) {
+      setFile(file);
+    }
+  };
+
+  const handleChangeImage = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    const files = event.target.files;
+    if (files && files[0] && validateFile(files[0])) {
+      setFile(files[0]);
+    }
+  };
+
+  const handleSubmit = () => {
+    // TODO: 이미지 업로드 기능 연결
   };
 
   return (
@@ -28,9 +68,21 @@ export default function ChangeProfileImageDialog() {
           <DialogTitle>프로필 이미지 수정</DialogTitle>
           <DialogDescription>사진을 선택하거나 드래그해 업로드할 수 있어요.</DialogDescription>
         </DialogHeader>
-        <ImageUploadDragArea onFile={handleFileUpload} onError={updateGlobalError}>
-          {/* TODO: 클릭을 통한 이미지 업로드 구현 */}
-        </ImageUploadDragArea>
+        {file ? (
+          <div>{/* TODO: 프로필 이미지 프리뷰 구현 */}</div>
+        ) : (
+          <ImageUploadDragArea onFile={handleDropImage} onError={updateGlobalError}>
+            <input
+              className="hidden"
+              id="profile-image"
+              type="file"
+              size={MAX_SIZE_EXCEEDED}
+              accept={ACCEPTED_IMAGE_TYPES.join(',')}
+              onChange={handleChangeImage}
+            />
+            <label htmlFor="profile-image">프로필 이미지 선택</label>
+          </ImageUploadDragArea>
+        )}
       </DialogContent>
     </Dialog>
   );
