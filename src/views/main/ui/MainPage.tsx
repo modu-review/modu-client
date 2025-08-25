1b56db490b264eea09edc30c60da9435a025301f
queenJiJi <lovebellaberry52@gmail.com>
2025-08-25 22:34:33 +0900

    [feat] mainpage의 최하단- contactUs 레이아웃 및 퍼블리싱 완료

--- src/views/main/ui/MainPage.tsx
+++ src/views/main/ui/MainPage.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/widgets/hero';
 import {getBestReviews} from '@/entities/reviews';
 import {BestReviews} from '@/features/reviews/best';
+import ContactUs from '@/widgets/contactus';
 
 export default async function MainPage() {
   const data = await getBestReviews();
@@ -10,7 +11,7 @@ export default async function MainPage() {
       <Hero />
       <BestReviews reviews={data} />
       <BestReviews reviews={data} />
-      <Hero />
+      <ContactUs />
     </section>
   );
 }
---
+++ src/widgets/contactus/ui/ContactUs.tsx
@@ -0,0 +1,24 @@
+export default function ContactUs() {
+  return (
+    <section className="flex justify-center items-center py-24 px-4 bg-[#f9fafb]">
+      <div className="w-full max-w-4xl rounded-[2rem] shadow-xl bg-white text-center py-20 px-6">
+        {/* Badge */}
+        <div className="inline-flex items-center justify-center px-4 py-1 rounded-full bg-[#ecfdf5] mb-6">
+          <span className="h-2 w-2 bg-green-500 rounded-full mr-2" />
+          <span className="text-sm font-medium text-[#065f46]">항시 대기중</span>
+        </div>
+
+        {/* Headline */}
+        <h2 className="text-2xl sm:text-xl font-bold text-gray-900 mb-6 leading-tight">
+          💬 후기는 모였고, 이제 아이디어 차례! <br />
+          사이트에 바라는 점이나 번뜩이는 의견이 있다면, 여기에 살포시 남겨주세요.
+        </h2>
+
+        {/* Button */}
+        <button className="mt-6 px-6 py-3 rounded-full border border-gray-400 hover:border-black transition text-lg font-medium">
+          Contact Us
+        </button>
+      </div>
+    </section>
+  );
+}
