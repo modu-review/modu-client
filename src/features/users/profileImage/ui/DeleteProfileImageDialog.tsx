3c3ec7fcd01e49f931b7a655d189774f67376bf8
GoJiMin <gojimin3095@gmail.com>
2025-08-25 22:50:19 +0900

    [style] 프로필 삭제 다이얼로그 스타일링, shadcn 다이얼로그의 디폴트 스타일 수정

--- src/features/users/profileImage/ui/DeleteProfileImageDialog.tsx
+++ src/features/users/profileImage/ui/DeleteProfileImageDialog.tsx
@@ -1,12 +1,15 @@
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from '@/shared/shadcnComponent/ui/dialog';
 import {LucideIcon} from '@/shared/ui/icons';
+import {PopoverClose} from '@radix-ui/react-popover';
 
 export default function DeleteProfileImageDialog() {
   return (
@@ -15,11 +18,29 @@ export default function DeleteProfileImageDialog() {
         <span>삭제</span>
         <LucideIcon name="Trash2" className="w-5 h-5 text-red-500" />
       </DialogTrigger>
-      <DialogContent className="w-[370px]">
+      <DialogContent className="w-[370px] pb-5 rounded-lg">
         <DialogHeader>
           <DialogTitle>프로필 이미지 삭제</DialogTitle>
           <DialogDescription>한 번 삭제한 프로필 이미지는 되돌릴 수 없어요.</DialogDescription>
         </DialogHeader>
+        <DialogFooter className="gap-2">
+          <DialogClose asChild>
+            <button
+              className="w-full py-1.5 bg-gray-300 text-black rounded-md hover:bg-gray-300/80 transition-colors"
+              aria-label="프로필 이미지 삭제 취소"
+            >
+              취소
+            </button>
+          </DialogClose>
+          <PopoverClose asChild>
+            <button
+              className="w-full text-center py-1.5 bg-boldBlue text-white rounded-md hover:bg-boldBlue/80 transition-colors"
+              aria-label="프로필 이미지 삭제"
+            >
+              삭제
+            </button>
+          </PopoverClose>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
