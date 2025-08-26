af05a4c03467c7c3be5fcb620ff3835a36c2a491
GoJiMin <gojimin3095@gmail.com>
2025-08-26 22:23:37 +0900

    [feat] SSE 이벤트 반환 타입 정의
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/entities/notifications/model/type.ts
+++ src/entities/notifications/model/type.ts
@@ -15,3 +15,17 @@ export type Notifications = {
   current_page: number;
   total_pages: number;
 };
+
+export type MetaEvent = {
+  hasNotification: boolean;
+};
+
+export type NotificationEvent = {
+  id: number;
+  board_id: number;
+  type: string;
+  title: string;
+  isRead: boolean;
+  isDeleted: boolean;
+  created_at: string;
+};
