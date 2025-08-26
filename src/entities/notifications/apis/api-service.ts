721535790fa55147a93ec60e9e6403b53e2c235d
GoJiMin <gojimin3095@gmail.com>
2025-08-26 21:41:34 +0900

    [feat] SSE 연결 전 토큰 검사를 위해 프리플라이트 요청 API 구현
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/entities/notifications/apis/api-service.ts
+++ src/entities/notifications/apis/api-service.ts
@@ -4,8 +4,8 @@ import {requestGet, requestPatch} from '@/shared/apis';
 export async function getNotifications(page: number) {
   return await requestGet<Notifications>({
     // TODO: 개발 완료 후 baseUrl 제거.
-    baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
-    endpoint: '/api/users/me/notifications',
+    // baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
+    endpoint: '/users/me/notifications',
     queryParams: {
       page: page,
     },
@@ -15,8 +15,8 @@ export async function getNotifications(page: number) {
 export async function markNotificationAsRead(notificationId: number) {
   await requestPatch({
     // TODO: 개발 완료 후 baseUrl 제거.
-    baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
-    endpoint: `/api/users/me/notifications/${notificationId}`,
+    // baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
+    endpoint: `/users/me/notifications/${notificationId}`,
     body: {
       isRead: true,
     },
@@ -26,10 +26,25 @@ export async function markNotificationAsRead(notificationId: number) {
 export async function deleteNotification(notificationId: number) {
   await requestPatch({
     // TODO: 개발 완료 후 baseUrl 제거.
-    baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
-    endpoint: `/api/users/me/notifications/${notificationId}`,
+    // baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
+    endpoint: `/users/me/notifications/${notificationId}`,
     body: {
       isDeleted: true,
     },
   });
+}
+
+export async function preflightNotifications() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/notifications/stream`, {
+    method: 'GET',
+    headers: {
+      Accept: 'text/event-stream',
+    },
+    credentials: 'include',
+  });
+
+  return {
+    ok: response.ok,
+    status: response.status,
+  };
 }
