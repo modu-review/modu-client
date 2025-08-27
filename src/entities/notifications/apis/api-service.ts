bd1f0c699832d2201ee0942579f9e5b6bbc1f135
GoJiMin <gojimin3095@gmail.com>
2025-08-27 19:07:34 +0900

    [remove] notifications 도메인 api-service의 불필요한 주석 제거
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/entities/notifications/apis/api-service.ts
+++ src/entities/notifications/apis/api-service.ts
@@ -3,8 +3,6 @@ import {requestGet, requestPatch} from '@/shared/apis';
 
 export async function getNotifications(page: number) {
   return await requestGet<Notifications>({
-    // TODO: 개발 완료 후 baseUrl 제거.
-    // baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
     endpoint: '/users/me/notifications',
     queryParams: {
       page: page,
@@ -14,8 +12,6 @@ export async function getNotifications(page: number) {
 
 export async function markNotificationAsRead(notificationId: number) {
   await requestPatch({
-    // TODO: 개발 완료 후 baseUrl 제거.
-    // baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
     endpoint: `/users/me/notifications/${notificationId}`,
     body: {
       isRead: true,
@@ -25,8 +21,6 @@ export async function markNotificationAsRead(notificationId: number) {
 
 export async function deleteNotification(notificationId: number) {
   await requestPatch({
-    // TODO: 개발 완료 후 baseUrl 제거.
-    // baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
     endpoint: `/users/me/notifications/${notificationId}`,
     body: {
       isDeleted: true,
