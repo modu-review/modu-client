2a8a3369b27465b39028125e4564e212f438d416
GoJiMin <gojimin3095@gmail.com>
2025-08-27 11:07:08 +0900

    [feat] 알림 페이지 이동 아이콘 헤더 배치
    - 현재 생성된 알림이 있을 경우 아이콘 위에 파란 원을 표시.
    - 아이콘 클릭 시 알림 페이지로 이동하며, 알림 상태를 false로 변경.
    
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/features/notifications/index.ts
+++ src/features/notifications/index.ts
@@ -1,1 +1,2 @@
 export {default as Notifications} from './ui/Notifications';
+export {default as NotificationBell} from './ui/NotificationBell';
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import Link from 'next/link';
-import {Sidebar} from '@/widgets/side-bar';
 import {usePathname} from 'next/navigation';
-import {LucideIcon} from '@/shared/ui/icons';
+import {Sidebar} from '@/widgets/side-bar';
+import {NotificationBell} from '@/features/notifications';
 
 export default function Header() {
   const pathName = usePathname();
@@ -17,12 +17,7 @@ export default function Header() {
         <h2 className="text-2xl md:text-3xl font-bold text-boldBlue">모두의 : 후기</h2>
       </Link>
       <div className="flex items-center gap-4 md:gap-6">
-        <Link href="/notifications">
-          <LucideIcon
-            name="Bell"
-            className="w-5 h-5 md:w-6 md:h-6 hover:text-boldBlue md:hover:scale-105 transition-all"
-          />
-        </Link>
+        <NotificationBell />
         <Sidebar />
       </div>
     </header>
