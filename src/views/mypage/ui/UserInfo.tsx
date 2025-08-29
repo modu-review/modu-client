e12c30e1039066221df2b67ebe9394fb81b8e44c
GoJiMin <gojimin3095@gmail.com>
2025-08-29 21:49:21 +0900

    [feat] 마이페이지에서 사용자 이메일을 표시하도록 변경

--- src/views/mypage/ui/UserInfo.tsx
+++ src/views/mypage/ui/UserInfo.tsx
@@ -2,11 +2,12 @@
 
 import {Suspense} from 'react';
 import {ProfileImage, ProfileImageLoading, EditProfileImage} from '@/features/users/profileImage';
-import {useUserNickname} from '@/entities/auth';
+import {useUserEmail, useUserNickname} from '@/entities/auth';
 import {Skeleton} from '@/shared/shadcnComponent/ui/skeleton';
 
 export default function UserInfo() {
   const userNickname = useUserNickname();
+  const userEmail = useUserEmail();
 
   return (
     <section className="flex flex-col items-center mt-10 md:mt-8 lg:mt-10">
@@ -25,6 +26,11 @@ export default function UserInfo() {
           <p className="text-xl lg:text-2xl font-bold">{userNickname}</p>
         ) : (
           <Skeleton className="h-5 md:h-6 lg:h-7 w-32 md:w-36 lg:w-40 mb-1 mt-2 md:mt-0" />
+        )}
+        {userEmail ? (
+          <p className="text-sm md:text-base font-semibold text-boldBlue">{userEmail}</p>
+        ) : (
+          <Skeleton className="h-4 md:h-5 lg:h-6 w-40 md:w-48 lg:w-52" />
         )}
       </div>
     </section>
