35a1bfaa78532a34b3e29765da6bc660b779823e
GoJiMin <gojimin3095@gmail.com>
2025-08-29 21:27:58 +0900

    [feat] /api/auth 라우트 핸들러에서 사용자 이메일 쿠키 조회 및 반환 값에 이메일 쿠키 value를 리턴하도록 변경

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -8,21 +8,29 @@ export async function GET() {
     return NextResponse.json({
       isLoggedIn: false,
       userNickname: null,
+      userEmail: null,
     });
   }
 
   const userNicknameCookie = cookieStore.get('userNickname');
+  const userEmailCookie = cookieStore.get('userEmail');
 
   if (!userNicknameCookie) {
     return NextResponse.json({errorCode: 'EMPTY_USER_NICKNAME'}, {status: 400});
   }
 
+  if (!userEmailCookie) {
+    return NextResponse.json({errorCode: 'EMPTY_USER_EMAIL'}, {status: 400});
+  }
+
   const userNickname = userNicknameCookie.value;
+  const userEmail = userEmailCookie.value;
 
   return NextResponse.json(
     {
       isLoggedIn: true,
       userNickname,
+      userEmail,
     },
     {status: 200},
   );
