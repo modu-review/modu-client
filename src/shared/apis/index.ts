4015705328e8f38d3ec0461cfac4433a848ffc54
Prolip <gojimin3095@gmail.com>
2026-01-22 20:39:04 +0900

    [refactor] 개별 테스트 작성을 위한 함수 분리
    - 각 함수를 독립적으로 테스트하기 위해 외부에 노출되는 래퍼 함수와 내부에서 사용되는 코어 함수를 분리.

--- src/shared/apis/index.ts
+++ src/shared/apis/index.ts
@@ -1,1 +1,94 @@
-export {requestGet, requestPost, requestPut, requestDelete, requestPatch} from './api-service';
+import {request} from './request-core';
+import {RequestMethodProps, WithErrorHandling} from './request-type';
+
+/**
+ * @description GET 메서드로 API 요청을 보내는 함수
+ * @param {RequestMethodProps} props - API 요청을 위한 프로퍼티 객체 (baseUrl, endpoint, headers, errorHandlingType)
+ * @returns {Promise<T>} - API 응답을 반환하는 Promise 객체
+ */
+export async function requestGet<T>({
+  headers = {},
+  errorHandlingType = 'errorBoundary',
+  withResponse = true,
+  ...args
+}: WithErrorHandling<RequestMethodProps>): Promise<T> {
+  return request<T>({
+    ...args,
+    method: 'GET',
+    headers,
+    withResponse,
+    errorHandlingType,
+  });
+}
+
+/**
+ * @description POST 메서드로 API 요청을 보내는 함수
+ * @param {RequestMethodProps} props - API 요청을 위한 프로퍼티 객체 (baseUrl, endpoint, headers, body)
+ * @returns {Promise<T>} - API 응답을 반환하는 Promise 객체
+ */
+export async function requestPost<T = void>({
+  headers = {},
+  withResponse = false,
+  ...args
+}: RequestMethodProps): Promise<T> {
+  return request<T>({
+    ...args,
+    method: 'POST',
+    headers,
+    withResponse,
+  });
+}
+
+/**
+ * @description PUT 메서드로 API 요청을 보내는 함수
+ * @param {RequestMethodProps} props - API 요청을 위한 프로퍼티 객체 (baseUrl, endpoint, headers, body)
+ * @returns {Promise<T>} - API 응답을 반환하는 Promise 객체
+ */
+export async function requestPut<T = void>({
+  headers = {},
+  withResponse = false,
+  ...args
+}: RequestMethodProps): Promise<T> {
+  return request<T>({
+    ...args,
+    method: 'PUT',
+    headers,
+    withResponse,
+  });
+}
+
+/**
+ * @description DELETE 메서드로 API 요청을 보내는 함수
+ * @param {RequestMethodProps} props - API 요청을 위한 프로퍼티 객체 (baseUrl, endpoint, headers)
+ * @returns {Promise<T>} - API 응답을 반환하는 Promise 객체
+ */
+export async function requestDelete<T = void>({
+  headers = {},
+  withResponse = false,
+  ...args
+}: RequestMethodProps): Promise<T> {
+  return request<T>({
+    ...args,
+    method: 'DELETE',
+    headers,
+    withResponse,
+  });
+}
+
+/**
+ * @description PATCH 메서드로 API 요청을 보내는 함수
+ * @param {RequestMethodProps} props - API 요청을 위한 프로퍼티 객체 (baseUrl, endpoint, headers, body)
+ * @returns {Promise<T>} - API 응답을 반환하는 Promise 객체
+ */
+export async function requestPatch<T = void>({
+  headers = {},
+  withResponse = false,
+  ...args
+}: RequestMethodProps): Promise<T> {
+  return request<T>({
+    ...args,
+    method: 'PATCH',
+    headers,
+    withResponse,
+  });
+}
