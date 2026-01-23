db61e5e4cb9ed19f8c3563baa797707ed422450f
Prolip <gojimin3095@gmail.com>
2026-01-24 01:39:13 +0900

    [test] NotificationItem 테스트 작성

---
+++ src/features/notifications/ui/test/NotificationItem.spec.tsx
@@ -0,0 +1,80 @@
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import mockRouter from 'next-router-mock';
+import {bookmarkNotificationStub, readCommentNotificationStub, unReadCommentNotificationStub} from './stub';
+import NotificationItem from '../NotificationItem';
+import {NOTIFICATION_CONFIG} from '../config/notification-config';
+
+const mockMarkNotificationAsRead = jest.fn();
+
+jest.mock('next/navigation', () => require('next-router-mock/navigation'));
+jest.mock('@/entities/notifications', () => ({
+  useMarkNotificationAsRead: () => ({markNotificationAsRead: mockMarkNotificationAsRead}),
+}));
+
+describe('src/features/notifications/ui/NotificationItem.tsx', () => {
+  beforeEach(() => {
+    mockMarkNotificationAsRead.mockReset();
+  });
+
+  it('컴포넌트가 렌더링된다.', () => {
+    const notification = bookmarkNotificationStub;
+    const config = NOTIFICATION_CONFIG[notification.type];
+    const ariaLabel = `${config.title} - ${config.getMessage(notification.title)} 게시글로 이동`;
+
+    render(<NotificationItem notification={notification} />);
+
+    expect(screen.getByLabelText(ariaLabel, {selector: 'button'})).toBeInTheDocument();
+    expect(screen.getByText(config.title)).toBeInTheDocument();
+    expect(screen.getByText(config.getMessage(notification.title))).toBeInTheDocument();
+    expect(screen.getByText(notification.created_at));
+  });
+
+  it('알림을 클릭하면 해당 게시글로 이동한다.', async () => {
+    const user = userEvent.setup();
+
+    const notification = bookmarkNotificationStub;
+    const config = NOTIFICATION_CONFIG[notification.type];
+    const ariaLabel = `${config.title} - ${config.getMessage(notification.title)} 게시글로 이동`;
+
+    render(<NotificationItem notification={notification} />);
+
+    const button = screen.getByLabelText(ariaLabel, {selector: 'button'});
+
+    await user.click(button);
+    expect(mockRouter.asPath).toBe(`/reviews/${notification.board_id}`);
+  });
+
+  it('읽지 않은 알림을 클릭하면 읽음 처리한다.', async () => {
+    const user = userEvent.setup();
+
+    const notification = unReadCommentNotificationStub;
+    const config = NOTIFICATION_CONFIG[notification.type];
+    const ariaLabel = `${config.title} - ${config.getMessage(notification.title)} 게시글로 이동`;
+
+    render(<NotificationItem notification={notification} />);
+
+    const button = screen.getByLabelText(ariaLabel, {selector: 'button'});
+
+    await user.click(button);
+
+    expect(mockMarkNotificationAsRead).toHaveBeenCalledTimes(1);
+    expect(mockMarkNotificationAsRead).toHaveBeenCalledWith(notification.id);
+  });
+
+  it('이미 읽은 알림을 클릭하면 읽음 처리하지 않는다.', async () => {
+    const user = userEvent.setup();
+
+    const notification = readCommentNotificationStub;
+    const config = NOTIFICATION_CONFIG[notification.type];
+    const ariaLabel = `${config.title} - ${config.getMessage(notification.title)} 게시글로 이동`;
+
+    render(<NotificationItem notification={notification} />);
+
+    const button = screen.getByLabelText(ariaLabel, {selector: 'button'});
+
+    await user.click(button);
+
+    expect(mockMarkNotificationAsRead).toHaveBeenCalledTimes(0);
+  });
+});
--- src/features/notifications/ui/test/stub.ts
+++ src/features/notifications/ui/test/stub.ts
@@ -10,7 +10,17 @@ export const bookmarkNotificationStub: Notification = {
   type: 'bookmark',
 };
 
-export const commentNotificationStub: Notification = {
+export const readCommentNotificationStub: Notification = {
+  id: 1,
+  board_id: 1,
+  isRead: true,
+  isDeleted: false,
+  title: '테스트',
+  created_at: '2026년 1월 24일',
+  type: 'bookmark',
+};
+
+export const unReadCommentNotificationStub: Notification = {
   id: 1,
   board_id: 1,
   isRead: false,
