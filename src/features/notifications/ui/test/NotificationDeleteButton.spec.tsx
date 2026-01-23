5b1975a8e486e852c43b19bdc4278b7d5d179937
Prolip <gojimin3095@gmail.com>
2026-01-24 01:07:55 +0900

    [test] NotificationDeleteButton 테스트 작성

---
+++ src/features/notifications/ui/test/NotificationDeleteButton.spec.tsx
@@ -0,0 +1,82 @@
+import {render, screen} from '@testing-library/react';
+import NotificationDeleteButton from '../NotificationDeleteButton';
+import userEvent from '@testing-library/user-event';
+import {useDeleteNotification} from '@/entities/notifications';
+
+jest.mock('@/entities/notifications');
+
+const mockUseDeleteNotification = useDeleteNotification as jest.MockedFunction<typeof useDeleteNotification>;
+const mockDeleteNotificationFn = jest.fn();
+
+describe('src/features/notifications/ui/NotificationDeleteButton.tsx', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    mockUseDeleteNotification.mockReturnValue({
+      deleteNotification: mockDeleteNotificationFn,
+      isPending: false,
+    } as unknown as ReturnType<typeof useDeleteNotification>);
+  });
+
+  it('컴포넌트가 렌더링되고 버튼이 활성화된다.', async () => {
+    const props = {
+      id: 1,
+      page: 1,
+      configTitle: '누군가 게시글을 저장했어요.',
+      configMessage: '테스트 게시글을 저장했어요!',
+    };
+    const ariaLabel = `${props.configTitle} - ${props.configMessage} 알림 삭제`;
+
+    render(<NotificationDeleteButton {...props} />);
+
+    const button = screen.getByLabelText(ariaLabel, {selector: 'button'});
+    expect(button).toBeInTheDocument();
+    expect(button).toBeEnabled();
+  });
+
+  it('삭제 버튼을 클릭하면 알림 삭제 요청을 보낸다.', async () => {
+    const user = userEvent.setup();
+    const props = {
+      id: 1,
+      page: 1,
+      configTitle: '누군가 게시글을 저장했어요.',
+      configMessage: '테스트 게시글을 저장했어요!',
+    };
+    const ariaLabel = `${props.configTitle} - ${props.configMessage} 알림 삭제`;
+
+    render(<NotificationDeleteButton {...props} />);
+
+    const button = screen.getByLabelText(ariaLabel, {selector: 'button'});
+    await user.click(button);
+
+    expect(mockDeleteNotificationFn).toHaveBeenCalledTimes(1);
+    expect(mockDeleteNotificationFn).toHaveBeenCalledWith({
+      notificationId: props.id,
+      page: props.page,
+    });
+  });
+
+  it('삭제 중일 때는 버튼이 비활성화 된다.', async () => {
+    mockUseDeleteNotification.mockReturnValue({
+      deleteNotification: mockDeleteNotificationFn,
+      isPending: true,
+    } as unknown as ReturnType<typeof useDeleteNotification>);
+
+    const user = userEvent.setup();
+    const props = {
+      id: 1,
+      page: 1,
+      configTitle: '누군가 게시글을 저장했어요.',
+      configMessage: '테스트 게시글을 저장했어요!',
+    };
+    const ariaLabel = `${props.configTitle} - ${props.configMessage} 알림 삭제`;
+
+    render(<NotificationDeleteButton {...props} />);
+
+    const button = screen.getByLabelText(ariaLabel, {selector: 'button'});
+    await user.click(button);
+
+    expect(mockDeleteNotificationFn).toHaveBeenCalledTimes(0);
+    expect(button).toBeDisabled();
+  });
+});
