77cc7dfa538716c0257764f6459303840475fd86
Prolip <gojimin3095@gmail.com>
2026-01-24 18:12:31 +0900

    [test] NotificationList 테스트 작성

--- src/features/notifications/ui/test/stub.ts
+++ src/features/notifications/ui/test/stub.ts
@@ -1,4 +1,4 @@
-import {Notification} from '@/entities/notifications';
+import {Notification, Notifications} from '@/entities/notifications';
 
 export const readNotificationStub: Notification = {
   id: 1,
@@ -18,4 +18,38 @@ export const unReadNotificationStub: Notification = {
   title: '테스트',
   created_at: '2026년 1월 24일',
   type: 'bookmark',
+};
+
+export const notificationsStub: Notifications = {
+  results: [
+    {
+      id: 1,
+      board_id: 1,
+      isRead: true,
+      isDeleted: false,
+      title: '테스트',
+      created_at: '2026년 1월 24일',
+      type: 'bookmark',
+    },
+    {
+      id: 2,
+      board_id: 2,
+      isRead: true,
+      isDeleted: false,
+      title: '테스트2',
+      created_at: '2026년 1월 24일',
+      type: 'comment',
+    },
+    {
+      id: 3,
+      board_id: 3,
+      isRead: true,
+      isDeleted: false,
+      title: '테스트2',
+      created_at: '2026년 1월 24일',
+      type: 'comment',
+    },
+  ],
+  current_page: 1,
+  total_pages: 1,
 };
