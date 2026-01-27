0abd142d3b7c1e5a5191f12cffdf8e2eb0e0b138
Prolip <gojimin3095@gmail.com>
2026-01-27 13:36:38 +0900

    [refactor] 최근 등록된 후기가 없을 때 표시되는 UI 추가

--- src/features/reviews/recent/ui/RecentReviewsCarousel.tsx
+++ src/features/reviews/recent/ui/RecentReviewsCarousel.tsx
@@ -1,26 +1,50 @@
 import RecentReviewCard from './RecentReviewCard';
 import MultiCarousel from './MultiCarousel';
 import {useGetRecentReviews} from '@/entities/reviews';
+import Link from 'next/link';
+import {Skeleton} from '@/shared/shadcnComponent/ui/skeleton';
 
 export default function RecentReviewsCarousel() {
   const {
     data: {latest_reviews},
   } = useGetRecentReviews();
 
+  if (false) {
+    return (
+      <section>
+        <p>아직 등록된 후기가 없어요.</p>
+      </section>
+    );
+  }
+
   return (
-    <div className="overflow-hidden w-full">
-      <MultiCarousel>
-        {latest_reviews.map(post => (
-          <RecentReviewCard key={`1-${post.board_id}`} post={post} />
-        ))}
-      </MultiCarousel>
-      <div className="hidden md:block mt-14">
-        <MultiCarousel rightToLeft={false}>
+    <section>
+      <div className="overflow-hidden w-full">
+        <MultiCarousel>
           {latest_reviews.map(post => (
-            <RecentReviewCard key={`2-${post.board_id}`} post={post} />
+            <RecentReviewCard key={`1-${post.board_id}`} post={post} />
           ))}
         </MultiCarousel>
+        <div className="hidden md:block mt-14">
+          <MultiCarousel rightToLeft={false}>
+            {latest_reviews.map(post => (
+              <RecentReviewCard key={`2-${post.board_id}`} post={post} />
+            ))}
+          </MultiCarousel>
+        </div>
       </div>
-    </div>
+      <div className="flex w-full mt-16 md:mt-24">
+        <Link
+          href="/search"
+          aria-label="더 많은 후기 보러가기"
+          className="relative inline-flex items-center justify-center mx-auto px-6 py-4 group transition-transform active:scale-95"
+        >
+          <span className="absolute inset-0 bg-lightBlue rounded-full transition-all duration-300 ease-in-out w-14 h-14 group-hover:w-full group-hover:bg-boldBlue"></span>
+          <span className="relative font-extrabold text-[16px] tracking-widest uppercase text-boldBlue group-hover:text-white">
+            더 많은 후기 보기{` >`}
+          </span>
+        </Link>
+      </div>
+    </section>
   );
 }
