f2795bdc623b559642d47faf5cbde32c22c84eea
Prolip <gojimin3095@gmail.com>
2026-01-27 14:11:51 +0900

    [refactor] 단위 테스트를 위한 컴포넌트 분리

--- src/features/reviews/recent/ui/RecentReviewsCarousel.tsx
+++ src/features/reviews/recent/ui/RecentReviewsCarousel.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
 import RecentReviewCard from './RecentReviewCard';
 import MultiCarousel from './MultiCarousel';
+import MoreReviewsLink from './MoreReviewsLink';
 import {useGetRecentReviews} from '@/entities/reviews';
 
 export default function RecentReviewsCarousel() {
@@ -32,18 +32,7 @@ export default function RecentReviewsCarousel() {
           </MultiCarousel>
         </div>
       </div>
-      <div className="flex w-full mt-16 md:mt-24">
-        <Link
-          href="/search"
-          aria-label="더 많은 후기 보러가기"
-          className="relative inline-flex items-center justify-center mx-auto px-6 py-4 group transition-transform active:scale-95"
-        >
-          <span className="absolute inset-0 bg-lightBlue rounded-full transition-all duration-300 ease-in-out w-14 h-14 group-hover:w-full group-hover:bg-boldBlue"></span>
-          <span className="relative font-extrabold text-[16px] tracking-widest uppercase text-boldBlue group-hover:text-white">
-            더 많은 후기 보기{` >`}
-          </span>
-        </Link>
-      </div>
+      <MoreReviewsLink />
     </section>
   );
 }
