a075fa54d67b4c0a99a79338ed98597b62ec81c6
Prolip <gojimin3095@gmail.com>
2026-02-04 14:30:44 +0900

    [test] 에디터 통합 테스트간 jsdom 문제 해결을 위해 setup 파일로 stub 처리 이관

--- jest.setup.tsx
+++ jest.setup.tsx
@@ -9,3 +9,33 @@ jest.mock('next/image', () => ({
 
 window.HTMLElement.prototype.hasPointerCapture = jest.fn();
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
+
+// https://stackoverflow.com/questions/68023284/react-testing-library-user-event-throws-error-typeerror-root-elementfrompoint/77219899#77219899
+// Tiptap이 기반하고 있는 Prosemirror가 참조하는 레이아웃 정보를 jsdom에서 지원하지 않음.
+// 해당 레이아웃 관련 정보를 stub 처리
+function getBoundingClientRect(): DOMRect {
+  const rec = {
+    x: 0,
+    y: 0,
+    bottom: 0,
+    height: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    width: 0,
+  };
+  return {...rec, toJSON: () => rec};
+}
+
+class FakeDOMRectList extends Array<DOMRect> implements DOMRectList {
+  item(index: number): DOMRect | null {
+    return this[index];
+  }
+}
+
+document.elementFromPoint = (): null => null;
+HTMLElement.prototype.getBoundingClientRect = getBoundingClientRect;
+HTMLElement.prototype.getClientRects = (): DOMRectList => new FakeDOMRectList();
+Range.prototype.getBoundingClientRect = getBoundingClientRect;
+Range.prototype.getClientRects = (): DOMRectList => new FakeDOMRectList();
+window.scrollBy = jest.fn();
--- src/views/review/edit/ui/test/EditReviewClient.spec.tsx
+++ src/views/review/edit/ui/test/EditReviewClient.spec.tsx
@@ -11,36 +11,6 @@ jest.mock('next/navigation', () => jest.requireActual('next-router-mock/navigati
 jest.mock('@/entities/review/apis/api-service');
 jest.mock('@/entities/auth');
 
-// https://stackoverflow.com/questions/68023284/react-testing-library-user-event-throws-error-typeerror-root-elementfrompoint/77219899#77219899
-// Tiptap이 기반하고 있는 Prosemirror가 참조하는 레이아웃 정보를 jsdom에서 지원하지 않음.
-// 해당 레이아웃 관련 정보를 stub 처리
-function getBoundingClientRect(): DOMRect {
-  const rec = {
-    x: 0,
-    y: 0,
-    bottom: 0,
-    height: 0,
-    left: 0,
-    right: 0,
-    top: 0,
-    width: 0,
-  };
-  return {...rec, toJSON: () => rec};
-}
-
-class FakeDOMRectList extends Array<DOMRect> implements DOMRectList {
-  item(index: number): DOMRect | null {
-    return this[index];
-  }
-}
-
-document.elementFromPoint = (): null => null;
-HTMLElement.prototype.getBoundingClientRect = getBoundingClientRect;
-HTMLElement.prototype.getClientRects = (): DOMRectList => new FakeDOMRectList();
-Range.prototype.getBoundingClientRect = getBoundingClientRect;
-Range.prototype.getClientRects = (): DOMRectList => new FakeDOMRectList();
-window.scrollBy = jest.fn();
-
 const mockPatchReview = patchReview as jest.MockedFunction<typeof patchReview>;
 const mockUseUserNickname = useUserNickname as jest.MockedFunction<typeof useUserNickname>;
 
