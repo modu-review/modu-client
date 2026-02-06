79e7c747435dddf03d0eca76e1678d15cb597740
Prolip <gojimin3095@gmail.com>
2026-02-06 16:30:35 +0900

    [feat] 알림 페이지에서 사용자 세션을 체크하도록 변경

--- src/views/notifications/ui/NotificationsPage.tsx
+++ src/views/notifications/ui/NotificationsPage.tsx
@@ -1,12 +1,15 @@
 import {Metadata} from 'next';
 import {Notifications} from '@/features/notifications';
+import checkSession from '@/shared/lib/utils/checkSession';
 
 export const metadata: Metadata = {
   title: '알림',
   description: '생성된 알림을 확인하세요.',
 };
 
-export default function NotificationsPage() {
+export default async function NotificationsPage() {
+  await checkSession();
+
   return (
     <section className="h-full flex flex-col max-w-5xl mx-auto md:px-4 pt-4 md:pt-7 md:pb-7">
       <h2 className="text-2xl font-semibold ml-6 mb-4 md:mb-6">알림</h2>
