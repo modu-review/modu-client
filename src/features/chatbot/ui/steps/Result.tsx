5f72e92298a927d5d6b6f49694fec628a2a1e9a5
Prolip <gojimin3095@gmail.com>
2026-02-13 02:07:18 +0900

    [feat] 검색 결과 단계에서 반환된 결과를 표시하도록 구현
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/features/chatbot/ui/steps/Result.tsx
+++ src/features/chatbot/ui/steps/Result.tsx
@@ -1,3 +1,34 @@
+import {
+  BotResponse,
+  ChatBubble,
+  FormattedSummary,
+  Step,
+  useChatStore,
+  useGetAIReviewSummary,
+} from '@/entities/ai-search';
+import {useShallow} from 'zustand/react/shallow';
+
 export default function Result() {
-  return <div>Result</div>;
+  const {keyword, category, goToInput, closeChat} = useChatStore(
+    useShallow(state => ({
+      keyword: state.keyword,
+      category: state.category,
+      goToInput: state.goToInput,
+      closeChat: state.closeChat,
+    })),
+  );
+
+  const {
+    data: {summary, sources},
+  } = useGetAIReviewSummary(keyword, category);
+
+  return (
+    <Step>
+      <BotResponse>
+        <ChatBubble>
+          <FormattedSummary text={summary} />
+        </ChatBubble>
+      </BotResponse>
+    </Step>
+  );
 }
