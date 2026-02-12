788ed2dd6666dcd8d5c6a48e89be6699bd9a5975
Prolip <gojimin3095@gmail.com>
2026-02-12 20:37:03 +0900

    [feat] 채팅창에서 전역 스토어의 단계 상태를 구독해 단계별로 UI를 표시하도록 구현
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/features/chatbot/ui/ChatWindow.tsx
+++ src/features/chatbot/ui/ChatWindow.tsx
@@ -1,9 +1,27 @@
+import Input from './steps/Input';
+import Ask from './steps/Ask';
+import Search from './steps/Search';
+import Loading from './steps/Loading';
+import Result from './steps/Result';
+import Error from './steps/Error';
+import {useChatStore} from '@/entities/ai-search/model/chatStore';
+
 export default function ChatWindow() {
+  const step = useChatStore(state => state.step);
+
   return (
     <section className="relative w-[500px] h-[700px] overflow-y-auto flex flex-col rounded-2xl shadow-2xl bg-neutral-100">
       <header className="sticky top-0 py-3 bg-white border-b flex justify-center items-center">
         <h3 className="font-bold text-lg text-gray-800">모후봇</h3>
       </header>
+      <section className="p-4 h-full">
+        {step === 'input' && <Input />}
+        {step === 'ask' && <Ask />}
+        {step === 'search' && <Search />}
+        {step === 'loading' && <Loading />}
+        {step === 'result' && <Result />}
+        {step === 'error' && <Error />}
+      </section>
     </section>
   );
 }
