0649d18c06903591057f5949b6c066b802a2afbe
Prolip <gojimin3095@gmail.com>
2026-02-12 23:49:35 +0900

    [fix] 카테고리가 선택되지 않은 경우 공용 suffix를 사용하기 위해 요청 인자 타입을 모두 Category 타입으로 변경
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/entities/ai-search/model/useGetAIReviewSummary.ts
+++ src/entities/ai-search/model/useGetAIReviewSummary.ts
@@ -1,7 +1,8 @@
 import {useSuspenseQuery} from '@tanstack/react-query';
 import {AISearchOptions} from './query-service';
 import {AISearchCategory} from './types';
+import {Category} from '@/entities/review';
 
-export function useGetAIReviewSummary(keyword: string, category: AISearchCategory) {
+export function useGetAIReviewSummary(keyword: string, category: Category) {
   return useSuspenseQuery(AISearchOptions.summary(keyword, category));
 }
