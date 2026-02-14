3923df7d120b52719a9847d90f94ca55146ebd72
Prolip <gojimin3095@gmail.com>
2026-02-15 01:43:45 +0900

    [feat] User 타입을 판별 유니온으로 변경, AuthProvider에서 세션 로드 후 검색 사용량 정보를 스토어에 업데이트하도록 추가
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/app/providers/AuthProvider.tsx
+++ src/app/providers/AuthProvider.tsx
@@ -2,6 +2,7 @@
 
 import {useEffect} from 'react';
 import {useGetSession, useUpdateUser} from '@/entities/auth';
+import {useChatStore} from '@/entities/ai-search';
 
 type Props = {
   children: React.ReactNode;
@@ -9,20 +10,29 @@ type Props = {
 
 export default function AuthProvider({children}: Props) {
   const {session, isSuccess} = useGetSession();
+  
   const updateUser = useUpdateUser();
+  const setChatLimit = useChatStore(state => state.setLimit);
 
   useEffect(() => {
-    if (session && session.isLoggedIn && session.userNickname) {
-      updateUser({
-        isLoggedIn: true,
-        userNickname: decodeURIComponent(session.userNickname),
-        userEmail: session.userEmail,
-      });
-    } else {
-      updateUser({
-        isLoggedIn: false,
-        userNickname: null,
-        userEmail: null,
+    if (session) {
+      if (session.isLoggedIn) {
+        updateUser({
+          isLoggedIn: true,
+          userNickname: decodeURIComponent(session.userNickname),
+          userEmail: session.userEmail,
+        });
+      } else {
+        updateUser({
+          isLoggedIn: false,
+          userNickname: null,
+          userEmail: null,
+        });
+      }
+      setChatLimit({
+        usage: session.searchLimit.usage,
+        maxLimit: session.searchLimit.maxLimit,
+        remaining: session.searchLimit.remaining,
       });
     }
   }, [updateUser, session, isSuccess]);
