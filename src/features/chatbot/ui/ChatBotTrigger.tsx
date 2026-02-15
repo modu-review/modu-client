b2765cd8158399eec88f0240fa0c02778d556a26
Prolip <gojimin3095@gmail.com>
2026-02-15 21:05:28 +0900

    [style] 모바일 환경의 챗봇 버튼, 플로팅 버튼 크기 수정
    Co-authored-by: queenJiJi <lovebellaberry52@gmail.com>

--- src/features/chatbot/ui/ChatBotTrigger.tsx
+++ src/features/chatbot/ui/ChatBotTrigger.tsx
@@ -8,12 +8,12 @@ type Props = {
 export default function ChatBotTrigger({isOpen, toogleChatBot}: Props) {
   return (
     <button
-      className={`w-14 h-14 rounded-full shadow-lg flex justify-center items-center transition-all duration-300 ease-in-out hover:scale-105 ${isOpen ? 'bg-gray-300' : 'bg-mediumBlue'}`}
+      className={`w-12 md:w-14 h-12 md:h-14 rounded-full shadow-lg flex justify-center items-center transition-all duration-300 ease-in-out hover:scale-105 ${isOpen ? 'bg-gray-300' : 'bg-mediumBlue'}`}
       aria-label={isOpen ? '챗봇 닫기' : '챗봇 열기'}
       onClick={toogleChatBot}
     >
       <LucideIcon
-        className={`w-8 h-8 transition-colors duration-300 ease-in-out ${isOpen ? 'text-gray-700' : 'text-white'}`}
+        className={`w-7 md:w-8 h-7 md:h-8 transition-colors duration-300 ease-in-out ${isOpen ? 'text-gray-700' : 'text-white'}`}
         name={isOpen ? 'BotOff' : 'Bot'}
       />
     </button>
--- src/views/search/ui/FloatingMoveUpButton.tsx
+++ src/views/search/ui/FloatingMoveUpButton.tsx
@@ -12,11 +12,14 @@ export default function FloatingMoveUpButton() {
 
   return (
     <button
-      className="w-14 h-14 rounded-full shadow-lg flex justify-center items-center transition-all duration-300 ease-in-out hover:scale-105 bg-mediumBlue"
+      className="w-12 md:w-14 h-12 md:h-14 rounded-full shadow-lg flex justify-center items-center transition-all duration-300 ease-in-out hover:scale-105 bg-mediumBlue"
       onClick={handleScrollToTop}
       aria-label="위로 이동"
     >
-      <LucideIcon className="w-8 h-8 transition-colors duration-300 ease-in-out text-white" name="ArrowUp" />
+      <LucideIcon
+        className="w-7 md:w-8 h-7 md:h-8 transition-colors duration-300 ease-in-out text-white"
+        name="ArrowUp"
+      />
     </button>
   );
 }
